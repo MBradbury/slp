4df1ff4f1b3375995a49a91eddf1be27621a6e2e
Matthew Bradbury <MBradbury@users.noreply.github.com>
2014-10-13 12:22:16 +0100

    Add argument parsing

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 
-import os, struct
+import os, sys, struct, importlib
 
 from simulator.Attacker import Attacker
 from simulator.Builder import build
@@ -13,35 +13,34 @@ from simulator.Simulation import Simulation
 def secureRandom():
 	return struct.unpack("<i", os.urandom(4))[0]
 
-module = "protectionless"
+module = sys.argv[1]
 
-gui = False
+Arguments = importlib.import_module("{}.Arguments".format(module))
 
-network_size = 11
-source_period = 1000
-wirelessRange = 45.0
+a = Arguments.Arguments()
+a.parse(sys.argv[2:])
 
-tfs_duration = 4000
-fake_period = 500
+build_arguments = a.getBuildArguments()
 
-configuration = CreateFurtherSinkCorner(network_size, wirelessRange - 2.5)
+configuration = CreateFurtherSinkCorner(a.args.network_size, a.args.wireless_range - 2.5)
 
-build(module,
-	SOURCE_PERIOD_MS=source_period,
-	SOURCE_NODE_ID=configuration.sourceId,
-	SINK_NODE_ID=configuration.sinkId,
-	TEMP_FAKE_DURATION_MS=tfs_duration,
-	FAKE_PERIOD_MS=fake_period,
-	ALGORITHM="GenericAlgorithm"
-)
+build_arguments.update(configuration.getBuildArguments())
+
+if configuration.spaceBehindSink:
+	build_arguments.update({"ALGORITHM": "GenericAlgorithm"})
+else:
+	build_arguments.update({"ALGORITHM": "FurtherAlgorithm"})
+
+# Now build the simulation with the specified arguments
+build(module, **build_arguments)
 
 seed = secureRandom()
 
-with Simulation(module, seed, configuration, wirelessRange, 30.0) as sim:
+with Simulation(module, seed, configuration, a.args) as sim:
 
 	sim.addAttacker(Attacker(sim, configuration.sourceId, configuration.sinkId))
 
-	if gui:
+	if a.args.mode == "GUI":
 		sim.setupGUI()
 
 	sim.run()
--- simulator/Simulation.py
+++ simulator/Simulation.py
@@ -3,16 +3,16 @@ import importlib
 from simulator.TosVis import TosVis
 
 class Simulation(TosVis):
-    def __init__(self, moduleName, seed, configuration, range, safetyPeriod=float('inf')):
+    def __init__(self, moduleName, seed, configuration, args):
 
         super(Simulation, self).__init__(
             importlib.import_module('{}.TOSSIM'.format(moduleName)),
             node_locations=configuration.topology.nodes,
-            range=range,
+            range=args.wireless_range,
             seed=seed
             )
 
-        self.safetyPeriod = safetyPeriod
+        self.safetyPeriod = args.safety_period
 
 #       self.tossim.addChannel("Metric-BCAST-Normal", sys.stdout)
 #       self.tossim.addChannel("Metric-RCV-Normal", sys.stdout)
