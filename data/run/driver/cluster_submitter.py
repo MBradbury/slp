28eccdd003e35ee169e4e891b3962a7e71c168a2
Matthew Bradbury <MBradbury@users.noreply.github.com>
2014-11-05 16:16:07 +0000

    Add missing cluster submitter

---
+++ data/run/driver/cluster_submitter.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+class Runner:
+    def __init__(self):
+        pass
+
+    def add_job(self, executable, options, name):
+        target_directory = name[:-len(".txt")]
+
+        word, space, options = options.partition(" ")
+
+        module = target_directory.replace("/", ".")
+
+        cluster_command = "qsub -q serial -j oe -V -l nodes=1:ppn=4 -l walltime=200:00:00 -N {}".format(module)
+
+        script_command = '{} {} {} > {}'.format(executable, module, options, name)
+
+        command = 'echo "cd \$PBS_O_WORKDIR ; {}" | {}'.format(script_command, cluster_command)
+
+        print(command)
+        #subprocess.check_call(command, shell=True)
+
+
+    def wait_for_completion(self):
+        pass
+
+    def fetch_results(self):
+        pass
+
+    def mode(self):
+        return "CLUSTER"
