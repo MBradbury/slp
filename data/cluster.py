a4bd9a9fd34ade4945a11e7d8952c5224618883b
Matthew Bradbury <matt-bradbury@live.co.uk>
2014-11-12 12:32:16 +0000

    Copy Analysis.py and Runner.py to the cluster build directory. Also disable loading the JDK as it doesn't work

--- data/cluster.py
+++ data/cluster.py
@@ -1,17 +1,24 @@
-import subprocess
+from __future__ import print_function
+import sys, subprocess
 
-def check_module_loaded(module):
+# These functions will only load modules in the subshell created.
+# They will not load modules into the parent shell.
+#
+# So make sure any necessary modules are loaded before submitting
+# jobs to the cluster!
+
+def module_loaded(module):
     output = subprocess.check_output("module list", shell=True, executable="/bin/bash")
-    if module not in output:
-        raise RuntimeError("The module {} is not loaded".format(module))
+    return module not in output:
 
 def load_module(module):
     try:
         subprocess.check_call("module load {}".format(module), shell=True, executable="/bin/bash")
 
     except subprocess.CalledProcessError as e:
-        print("Failed to load {}: {}".format(module, e))
-        print("Checking that {} is loaded:".format(module))
+        print("Failed to load {}: {}".format(module, e), file=sts.stderr)
+        print("Checking that {} is loaded:".format(module), file=sts.stderr)
 
     finally:
-        check_module_loaded(module)
+        if not module_loaded(module):
+        	raise RuntimeError("The module {} is not loaded".format(module))
