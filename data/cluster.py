8a0a0b5a219846d781eaee22181a7161af929971
Matthew Bradbury <MBradbury@users.noreply.github.com>
2014-11-12 12:28:45 +0000

    Improve the load_module function so that it doesn't fail to find the module command

--- data/cluster.py
+++ data/cluster.py
@@ -1,13 +1,17 @@
 import subprocess
 
+def check_module_loaded(module):
+    output = subprocess.check_output("module list", shell=True, executable="/bin/bash")
+    if module not in output:
+        raise RuntimeError("The module {} is not loaded".format(module))
+
 def load_module(module):
     try:
-        subprocess.check_call("module load {}".format(module), shell=True)
+        subprocess.check_call("module load {}".format(module), shell=True, executable="/bin/bash")
+
     except subprocess.CalledProcessError as e:
         print("Failed to load {}: {}".format(module, e))
         print("Checking that {} is loaded:".format(module))
 
-        # Could not load the module, so check that it is loaded
-        output = subprocess.check_output("module list", shell=True)
-        if module not in output:
-            raise RuntimeError("The module {} is not loaded".format(module))
+    finally:
+        check_module_loaded(module)
