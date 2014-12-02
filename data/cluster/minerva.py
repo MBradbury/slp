7d1a4aa1de349817dcf337ffef9a7d5d0f52d7a9
Matthew Bradbury <matt-bradbury@live.co.uk>
2014-12-02 16:28:14 +0000

    Instead of having a fixed memory limit for minerva scripts. ask for 1 GB per job

--- data/cluster/minerva.py
+++ data/cluster/minerva.py
@@ -21,7 +21,9 @@ def submitter():
     from data.run.driver.cluster_submitter import Runner as Submitter
 
     # Size 25 network seem to take ~500mb per instance, so use 1000mb per instance to be safe
-    cluster_command = "msub -q smp -j oe -l nodes=1:ppn={} -l walltime=30:00:00 -l mem=4000mb -N {{}}".format(ppn())
+    ram_per_job_mb = 1000
+
+    cluster_command = "msub -q smp -j oe -l nodes=1:ppn={} -l walltime=25:00:00 -l mem={}mb -N {{}}".format(ppn(), ppn() * ram_per_job_mb)
 
     prepare_command = "module swap oldmodules minerva-2.0 ; module load iomkl/13.1.3/ScientificPython/2.8-python-2.7.6"
     
