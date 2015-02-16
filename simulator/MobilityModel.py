1cf6c63e5d2802f6578aa7a9d83008a4870dde95
Matthew Bradbury <MBradbury@users.noreply.github.com>
2015-02-16 16:12:15 +0000

    Add a walk to sink mobility model.

--- simulator/MobilityModel.py
+++ simulator/MobilityModel.py
@@ -6,25 +6,24 @@ class MobilityModel(object):
 
 		self._validate_times()
 
-	def _validate_times(self, times):
+	def _validate_times(self):
 		"""Checks if the list of time intervals is valid"""
 
 		num_nodes = len(self.configuration.topology.nodes)
 
-		for (node_id, intervals) in self.active_times:
+		for (node_id, intervals) in self.active_times.items():
 			if node_id > num_nodes:
 				raise RuntimeError("Invalid node id {}.".format(node_id))
 
 class StationaryMobilityModel(MobilityModel):
 	def __init__(self, configuration):
 
-		times = { configuration.source_id: [(0, float('inf')] }
+		times = { configuration.source_id: [(0, float('inf'))] }
 
-		super(StationaryMobility, self).__init__(configuration, times)
+		super(StationaryMobilityModel, self).__init__(configuration, times)
 
 class RandomWalkMobilityModel(MobilityModel):
-	def __init__(self, configuration, duration):
-		super(RandomWalkMobilityModel, self).__init__(configuration)
+	def __init__(self, configuration, max_time, duration=1):
 
 		# Duration is the length for which a node will act as a source node.
 		self.duration = duration
@@ -32,10 +31,22 @@ class RandomWalkMobilityModel(MobilityModel):
 		# TODO: Use configuration.connectivity_matrix to choose which node
 		# to move the source to after the duration
 
+		super(RandomWalkMobilityModel, self).__init__(configuration)
+
 
 class TowardsSinkMobilityModel(MobilityModel):
-	def __init__(self, configuration):
-		super(TowardsSinkMobilityModel, self).__init__(configuration)
+	def __init__(self, configuration, duration=1):
+		path = configuration.shortest_path(configuration.source_id, configuration.sink_id)
 
-		# TODO: Use scipy to find shortest path using configuration.connectivity_matrix
+		times = {}
 
+		current_time = 0
+
+		for (i, node) in enumerate(path):
+			end_time = current_time + duration if (i + 1) != len(path) else float('inf')
+
+			times[node] = [(current_time, end_time)]
+
+			current_time += duration
+
+		super(TowardsSinkMobilityModel, self).__init__(configuration, times)
