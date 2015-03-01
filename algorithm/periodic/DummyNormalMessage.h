f16bae87a32176c2642e8b4234aa0064e4ea0905
Matthew Bradbury <MBradbury@users.noreply.github.com>
2015-03-01 09:10:05 +0000

    Add missing DummyMessage.h

---
+++ algorithm/periodic/DummyNormalMessage.h
@@ -0,0 +1,13 @@
+#ifndef SLP_MESSAGES_DUMMYNORMALMESSAGE_H
+#define SLP_MESSAGES_DUMMYNORMALMESSAGE_H
+
+#include "SequenceNumber.h"
+#include "NormalMessage.h"
+
+typedef nx_struct DummyNormalMessage {
+		nx_uint64_t sequence_number;
+		nx_uint8_t state[sizeof(NormalMessage) - sizeof(nx_uint64_t)];
+
+} DummyNormalMessage;
+
+#endif // SLP_MESSAGES_DUMMYNORMALMESSAGE_H
