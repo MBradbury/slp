ce4b4ca0b5304cdc765c2f5b6670511d2727e984
Matthew Bradbury <matt-bradbury@live.co.uk>
2015-03-05 12:47:59 +0000

    Tidy up phantom runner script to work towards being able to run phantom

--- algorithm/phantom/Runner.py
+++ algorithm/phantom/Runner.py
@@ -1,39 +1,66 @@
-import subprocess
-import os
-import itertools
+import os, itertools
+
+from collections import OrderedDict
 
 from data.run.common import RunSimulationsCommon
 
 class RunSimulations(RunSimulationsCommon):
-    def __init__(self, driver, results_directory, skip_completed_simulations=True):
+    def __init__(self, driver, results_directory, safety_periods, skip_completed_simulations=True):
         super(RunSimulations, self).__init__(driver, results_directory, skip_completed_simulations)
+        self.safety_periods = safety_periods
 
-    def run(self, exe_path, distance, sizes, source_periods, configurations, repeats):
+    def run(self, exe_path, distance, sizes, source_periods, walk_hop_lengths, configurations, repeats):
         if self.skip_completed_simulations:
             self._check_existing_results(['network_size', 'source_period', 'configuration'])
         
         if not os.path.exists(exe_path):
             raise Exception("The file {} doesn't exist".format(exe_path))
 
-        for (size, source_period, configuration) in itertools.product(sizes, source_periods, configurations):
-            if not self._already_processed(repeats, size, source_period, configuration):
+        # TODO Pass these along properly
+        retries = 5
+        delay = 0.050
+
+        for (size, source_period, walk_length, configuration) in itertools.product(sizes, source_periods, walk_hop_lengths, configurations):
+            if not self._already_processed(repeats, size, source_period, walk_length, configuration):
+
+                try:
+                    safety_period = 0 if self.safety_periods is None else self.safety_periods[configuration][size][source_period]
+                except KeyError as e:
+                    raise KeyError("Failed to find the key {}".format((configuration, size, source_period)), e)
 
                 executable = 'python {} {}'.format(
                     self.optimisations,
                     exe_path)
 
-                options = 'algorithm.phantom --mode {} --network-size {} --source-period {} --configuration {} --job-size {} --distance {}'.format(
-                    self.driver.mode(),
-                    size,
-                    source_period,
-                    configuration,
-                    repeats,
-                    distance)
+                opts = OrderedDict()
+                opts["--mode"] = self.driver.mode()
+                opts["--network-size"] = size
+                opts["--configuration"] = configuration
+                opts["--safety-period"] = safety_period
+                opts["--source-period"] = source_period
+                opts["--random-walk-hops"] = walk_length
+                opts["--random-walk-retries"] = retries
+                opts["--random-walk-delay"] = delay
+                opts["--distance"] = distance
+                opts["--job-size"] = repeats
 
-                filename = os.path.join(self.results_directory, '{}-{}-{}-{}'.format(
+                optItems = ["{} {}".format(k, v) for (k,v) in opts.items()]
+
+                options = 'algorithm.phantom ' + " ".join(optItems)
+
+                filenameItems = (
                     size,
-                    source_period,
                     configuration,
-                    distance).replace(".", "_") + ".txt")
+                    source_period,
+                    walk_length,
+                    retries,
+                    delay,
+                    distance
+                )
+
+                filename = os.path.join(
+                    self.results_directory,
+                    '-'.join(map(str, filenameItems)).replace(".", "_") + ".txt"
+                )
 
                 self.driver.add_job(executable, options, filename)
