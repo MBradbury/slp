6ed7dab89ca082fd676d6222ca932fcd7693427d
Matthew Bradbury <MBradbury@users.noreply.github.com>
2015-04-28 19:35:21 +0100

    Experiment with an attacker that ignored messages for a certain amount of time after moving due to a message

--- simulator/Attacker.py
+++ simulator/Attacker.py
@@ -111,6 +111,38 @@ class IgnorePreviousLocationReactiveAttacker(Attacker):
         self.previous_location = self.position
         super(IgnorePreviousLocationReactiveAttacker, self).move(node_id)
 
+
+class TimeSensitiveReactiveAttacker(Attacker):
+    def __init__(self, sim, start_node_id, wait_time=0.010):
+        super(TimeSensitiveReactiveAttacker, self).__init__(sim, start_node_id)
+        self.previous_location = None
+        self.last_moved_time = None
+        self.wait_time = wait_time
+
+    def process(self, line):
+        # Don't want to move if the source has been found
+        if self.found_source():
+            return
+
+        (time, msg_type, node_id, prox_from_id, ult_from_id, sequence_number) = self._process_line(line)
+
+        if self.last_moved_time is not None and abs(time - self.last_moved_time) < self.wait_time:
+            return
+
+        if self.position == node_id and \
+            (msg_type in _messages_without_sequence_numbers or
+            self.previous_location != prox_from_id):
+
+            self.last_moved_time = time
+            self.move(prox_from_id)
+
+            self.draw(time, self.position)
+
+    def move(self, node_id):
+        self.previous_location = self.position
+        super(TimeSensitiveReactiveAttacker, self).move(node_id)
+
+
 # This attacker can determine the type of a message and its sequence number,
 # but is unaware of the ultimate source of the message.
 class SeqNoReactiveAttacker(Attacker):
