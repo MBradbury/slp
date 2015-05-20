32b5c090acd4bfefba387fee2cfaf79d9815c501
Matthew Bradbury <MBradbury@users.noreply.github.com>
2015-05-20 13:07:29 +0100

    PhantomReliable: Correctly name this algorithm

--- algorithm/phantom_reliable/__init__.py
+++ algorithm/phantom_reliable/__init__.py
@@ -2,6 +2,6 @@ import Analysis, Arguments, CommandLine, Runner, Metrics
 
 def _setup():
 	import algorithm
-	return algorithm._setup_algorithm_paths("phantom")
+	return algorithm._setup_algorithm_paths("phantom_reliable")
 
 (name, results_path, result_file, result_file_path, graphs_path) = _setup()
