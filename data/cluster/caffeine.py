4b4b392b55b45cf8c81e50f6b08048881fd3ae3b
Matthew Bradbury <MBradbury@users.noreply.github.com>
2015-06-02 15:50:38 +0100

    Attempt to fix caffeine execution issues

--- data/cluster/caffeine.py
+++ data/cluster/caffeine.py
@@ -28,6 +28,6 @@ def submitter():
     # The -h flags causes the jobs to be submitted as held. It will need to be released before it is run.
     cluster_command = "qsub -q blend -j oe -h -l nodes=1:ppn={} -l walltime=500:00:00 -l mem={}mb -N \"{{}}\"".format(ppn(), ppn() * ram_per_job_mb)
 
-    prepare_command = "module load jdk/1.7.0_07 ; module load python/2.7.8"
+    prepare_command = "module load jdk/1.7.0_07 ; module load python/2.7.8 ; LD_LIBRARY_PATH=\"$LD_LIBRARY_PATH:/opt/share/lib\""
 
     return Submitter(cluster_command, prepare_command)
