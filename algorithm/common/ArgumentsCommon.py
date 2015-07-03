6c6258960a7e043c43cea50198f7888bfa7bf0ae
Matthew Bradbury <MBradbury@users.noreply.github.com>
2015-07-03 20:36:53 +0100

    Add short name for commonly used arguments and add an error check when using configurations with multiple sources with algorithms that only support a single source

--- algorithm/common/ArgumentsCommon.py
+++ algorithm/common/ArgumentsCommon.py
@@ -10,17 +10,17 @@ class ArgumentsCommon(object):
 
         parser.add_argument("--seed", type=int)
 
-        parser.add_argument("--communication-model", type=str, choices=Simulator.available_communication_models(), required=True)
-        parser.add_argument("--noise-model", type=str, choices=Simulator.available_noise_models(), required=True)
+        parser.add_argument("-cm", "--communication-model", type=str, choices=Simulator.available_communication_models(), required=True)
+        parser.add_argument("-nm", "--noise-model", type=str, choices=Simulator.available_noise_models(), required=True)
 
-        parser.add_argument("--network-size", type=int, required=True)
-        parser.add_argument("--distance", type=float, default=4.5)
-        parser.add_argument("--configuration", type=str, required=True, choices=Configuration.names())
+        parser.add_argument("-ns", "--network-size", type=int, required=True)
+        parser.add_argument("-d", "--distance", type=float, default=4.5)
+        parser.add_argument("-c", "--configuration", type=str, required=True, choices=Configuration.names())
 
-        parser.add_argument("--attacker-model", type=Attacker.eval_input, default=Attacker.default())
+        parser.add_argument("-am", "--attacker-model", type=Attacker.eval_input, default=Attacker.default())
 
         if has_safety_period:
-            parser.add_argument("--safety-period", type=float, required=True)
+            parser.add_argument("-safety", "--safety-period", type=float, required=True)
 
         parser.add_argument("--job-size", type=int, default=1)
         parser.add_argument("--thread-count", type=int, default=multiprocessing.cpu_count())
@@ -57,6 +57,10 @@ class ArgumentsCommon(object):
             result.update(self.args.source_mobility.build_arguments())
         else:
             configuration = Configuration.create(self.args.configuration, self.args)
+
+            if len(configuration.source_ids) != 1:
+                raise RuntimeError("Invalid number of source ids in configuration {}, there must be exactly one.".format(configuration))
+
             result["SOURCE_NODE_ID"] = list(configuration.source_ids)[0]
 
         return result
