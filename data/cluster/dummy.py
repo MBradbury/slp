6fa2d7f04b997af7f3e5964ba695a5414ca0c5ee
Matthew Bradbury <matt-bradbury@live.co.uk>
2015-07-03 19:38:36 +0100

    Fix the dummy cluster submitter

--- data/cluster/dummy.py
+++ data/cluster/dummy.py
@@ -15,14 +15,11 @@ def copy_to():
 def copy_back(dirname):
     raise RuntimeError("Cannot copy back from the dummy cluster")
 
-def submitter():
+def submitter(notify_emails=None):
     from data.run.driver.cluster_submitter import Runner as Submitter
 
     # Don't submit, just print the command
     class DummySubmitter(Submitter):
-        def __init__(self, cluster_command, prepare_command):
-            super(DummySubmitter, self).__init__(cluster_command, prepare_command)
-
         def _submit_job(self, command):
             print(command)
 
@@ -30,6 +27,9 @@ def submitter():
 
     cluster_command = "qsub -q serial -j oe -h -l nodes=1:ppn={} -l walltime=500:00:00 -l mem={}mb -N \"{{}}\"".format(ppn(), ppn() * ram_per_job_mb)
 
+    if notify_emails is not None and len(notify_emails) > 0:
+        cluster_command += " -m ae -M {}".format(",".join(notify_emails))
+
     prepare_command = " <prepare> "
 
-    return DummySubmitter(cluster_command, prepare_command)
+    return DummySubmitter(cluster_command, prepare_command, ppn())
