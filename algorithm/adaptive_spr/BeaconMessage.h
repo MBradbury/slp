5feb2d7fd3bfe3ee24aae8e44b4aa2f4769f406f
Matthew Bradbury <matt-bradbury@live.co.uk>
2015-07-12 16:38:26 +0100

    AdaptiveSpr: Add the beacon message header file

---
+++ algorithm/adaptive_spr/BeaconMessage.h
@@ -0,0 +1,14 @@
+#ifndef SLP_MESSAGES_BEACONMESSAGE_H
+#define SLP_MESSAGES_BEACONMESSAGE_H
+
+typedef nx_struct BeaconMessage
+{
+  nx_int16_t source_distance_of_sender;
+  nx_uint8_t padding[22];
+
+} BeaconMessage;
+
+inline int64_t Beacon_get_sequence_number(const BeaconMessage* msg) { return BOTTOM; }
+inline int32_t Beacon_get_source_id(const BeaconMessage* msg) { return BOTTOM; }
+
+#endif // SLP_MESSAGES_BEACONMESSAGE_H
