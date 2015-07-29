6172b223b5bc2d711b1c802e986589ec330c2b30
Matthew Bradbury <MBradbury@users.noreply.github.com>
2015-07-29 12:26:35 +0100

    Add NoForward that provides SLP by not forwarding messages

---
+++ algorithm/noforward/Arguments.py
@@ -0,0 +1,15 @@
+import argparse
+from algorithm.common.ArgumentsCommon import ArgumentsCommon
+import simulator.SourcePeriodModel
+import simulator.MobilityModel
+
+class Arguments(ArgumentsCommon):
+    def __init__(self):
+        parser = argparse.ArgumentParser(description="SLP No Forward", add_help=True)
+        super(Arguments, self).__init__(parser, has_safety_period=True)
+
+        parser.add_argument("--source-period",
+            type=simulator.SourcePeriodModel.eval_input, required=True)
+        parser.add_argument("--source-mobility",
+            type=simulator.MobilityModel.eval_input,
+            default=simulator.MobilityModel.StationaryMobilityModel())
