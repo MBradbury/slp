808cfcdd549072eff293f7af06f0867e10661994
Matthew Bradbury <matt-bradbury@live.co.uk>
2015-07-29 14:05:13 +0100

    Modify the builder so that the stderro is also routed to the main stderr

--- simulator/Builder.py
+++ simulator/Builder.py
@@ -5,13 +5,14 @@ def build(directory, **kwargs):
 
     flags = " ".join("-D{}={}".format(k, repr(v)) for (k, v) in kwargs.items())
 
-    command = 'make micaz sim safe SLP_PARAMETER_CFLAGS="{}"'.format(flags)
+    command = 'make micaz sim SLP_PARAMETER_CFLAGS="{}"'.format(flags)
 
     result = subprocess.check_call(
         command,
         cwd=directory,
         shell=True,
-        stdout=sys.stderr.fileno()
+        stdout=sys.stderr,
+        stderr=sys.stderr
         )
 
     return result
