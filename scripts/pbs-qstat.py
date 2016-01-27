eb60f87a5b704b846ac8f57de943e9c64858e03d
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-01-27 10:08:54 +0000

    Use name == main idiom in the qstat scripts

--- scripts/pbs-qstat.py
+++ scripts/pbs-qstat.py
@@ -5,20 +5,6 @@ from __future__ import print_function
 import xml.dom.minidom
 import os, sys, subprocess
 
-# The caller can specify a username if they wish,
-# or we can do the sensible thing and guess they want
-# to find info out about their own jobs
-try:
-    username = sys.argv[1]
-except IndexError:
-    username = os.environ['USER']
-
-qstat_output = subprocess.check_output('qstat -x', shell=True)
-
-dom = xml.dom.minidom.parseString(qstat_output)
-
-jobs = dom.getElementsByTagName('Job')
-
 def fakeqstat(joblist):
     for job in joblist:
         job_id = job.getElementsByTagName('Job_Id')[0].childNodes[0].data
@@ -30,4 +16,19 @@ def fakeqstat(joblist):
         if username in job_owner:
             print(job_id, job_state, job_name)     
 
-fakeqstat(jobs)
+if __name__ == '__main__':
+	# The caller can specify a username if they wish,
+	# or we can do the sensible thing and guess they want
+	# to find info out about their own jobs
+	try:
+	    username = sys.argv[1]
+	except IndexError:
+	    username = os.environ['USER']
+
+	qstat_output = subprocess.check_output('qstat -x', shell=True)
+
+	dom = xml.dom.minidom.parseString(qstat_output)
+
+	jobs = dom.getElementsByTagName('Job')
+
+	fakeqstat(jobs)
--- scripts/sge-qstat.py
+++ scripts/sge-qstat.py
@@ -5,23 +5,6 @@ from __future__ import print_function
 import xml.dom.minidom
 import os, sys, subprocess
 
-# The caller can specify a username if they wish,
-# or we can do the sensible thing and guess they want
-# to find info out about their own jobs
-try:
-    username = sys.argv[1]
-except IndexError:
-    username = os.environ['USER']
-
-qstat_output = subprocess.check_output('qstat -xml -r', shell=True)
-
-dom = xml.dom.minidom.parseString(qstat_output)
-
-jobs = dom.getElementsByTagName('job_info')
-run = jobs[0]
-
-runjobs = run.getElementsByTagName('job_list')
-
 def fakeqstat(joblist):
     for job in joblist:
         jobname = job.getElementsByTagName('JB_name')[0].childNodes[0].data
@@ -40,4 +23,22 @@ def fakeqstat(joblist):
         if username in jobown:
             print(jobnum, '\t', jobown.ljust(16), '\t', jobname.ljust(16), '\t', jobstate, '\t', jobtime)
 
-fakeqstat(runjobs)
+if __name__ == '__main__':
+    # The caller can specify a username if they wish,
+    # or we can do the sensible thing and guess they want
+    # to find info out about their own jobs
+    try:
+        username = sys.argv[1]
+    except IndexError:
+        username = os.environ['USER']
+
+    qstat_output = subprocess.check_output('qstat -xml -r', shell=True)
+
+    dom = xml.dom.minidom.parseString(qstat_output)
+
+    jobs = dom.getElementsByTagName('job_info')
+    run = jobs[0]
+
+    runjobs = run.getElementsByTagName('job_list')
+
+    fakeqstat(runjobs)
