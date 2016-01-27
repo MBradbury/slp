531ab097cc3fef4fdda035007a03d7f9f9ad3b4a
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-01-27 09:58:16 +0000

    Tweak summary graph generation to fit more graphs onto a single page

--- data/graph/summary.py
+++ data/graph/summary.py
@@ -6,24 +6,23 @@ import data.util
 from data import latex
 
 class GraphSummary:
-    def __init__(self, directory, name, graphs_on_page=2):
-        self.directory = directory
-        self.name = name
-        self.graphs_on_page = graphs_on_page
-        self.graph_count = 0
+    def __init__(self, directory, name, graphs_on_page=6):
+        self._directory = directory
+        self._name = name
+        self._graphs_on_page = graphs_on_page
+        self._graph_count = 0
+
+        self.width_factor = 0.9
+        self.height = None
 
     def _write_latex_header(self, stream):
-        stream.write("\\documentclass{article}\n")
-        stream.write("\\usepackage{fullpage}\n")
-        stream.write("\\usepackage[margin=0.25in,portrait]{geometry}\n")
-        stream.write("\\usepackage{boxedminipage}\n")
-        stream.write("\\usepackage{subfig}\n")
+        stream.write("\\documentclass[twocolumn]{article}\n")
+        stream.write("\\usepackage[margin=0.2in,portrait]{geometry}\n")
         stream.write("\\usepackage{graphicx}\n")
-        stream.write("\\usepackage{grffile}\n")
-        stream.write("\\usepackage{morefloats}\n")
+        stream.write("\\usepackage{grffile}\n") # Long file names
+        stream.write("\\usepackage{morefloats}\n") # More floats with no text
         stream.write("\\usepackage{framed}\n")
-        stream.write("\\usepackage[justification=centering]{caption}\n")
-        stream.write("\\pdfoptionpdfminorversion=5\n")
+        stream.write("\\usepackage{caption}\n")
         stream.write("\\begin{document}\n")
 
     def _write_latex_footer(self, stream):
@@ -36,25 +35,30 @@ class GraphSummary:
 
         image_path = os.path.join(directory, name_without_ext + '.pdf').replace('\\', '/')
 
-        stream.write("  \\begin{figure}[H]\n")
+        stream.write("  \\begin{figure}\n")
         stream.write("  \\begin{framed}\n")
-        #stream.write("   \\centering"
-        stream.write("    \\includegraphics{{{0}}}\n".format(image_path))
+        #stream.write("   \\centering\n")
+
+        if self.width_factor:
+            stream.write("    \\includegraphics[width={0}\\textwidth]{{{1}}}\n".format(self.width_factor, image_path))
+        elif self.height:
+            stream.write("    \\includegraphics[height={0}]{{{1}}}\n".format(self.height, image_path))
+
         stream.write("    \\caption[justification=centering]{{\\small {0} }}\n".format(caption))
         stream.write("  \\end{framed}\n")
         stream.write("  \\end{figure}\n")
 
-        self.graph_count += 1
+        self._graph_count += 1
 
-        if self.graph_count % self.graphs_on_page == 0:
-            stream.write("  \\clearpage\n")
+        if self._graphs_on_page is not None and self._graph_count % self._graphs_on_page == 0:
+           stream.write("  \\clearpage\n")
 
     def create(self):
-        with open(self.name + '.tex', 'w') as output_latex:
+        with open(self._name + '.tex', 'w') as output_latex:
 
             self._write_latex_header(output_latex)
 
-            walk_dir = self.directory
+            walk_dir = self._directory
             print("Summary: Looking for graphs in {}".format(walk_dir))
 
             for (root, subdirs, files) in os.walk(walk_dir):
@@ -71,15 +75,15 @@ class GraphSummary:
 
 
     def compile(self):
-        latex.compile_document('{}.tex'.format(self.name))
+        latex.compile_document('{}.tex'.format(self._name))
 
     def clean(self):
         extensions = ['.pdf', '.tex']
         for extension in extensions:
-            data.util.silent_remove(self.name + extension)
+            data.util.silent_remove(self._name + extension)
 
     def run(self):
-        self.graph_count = 0
+        self._graph_count = 0
         self.clean()
         self.create()
         self.compile()
