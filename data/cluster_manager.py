ccf076a9896e899c53b27064e34c3c8671bf1877
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-02-01 18:58:03 +0000

    Remove code intended to load cluster dependancies, but never worked and probably never will

--- data/cluster_manager.py
+++ data/cluster_manager.py
@@ -1,30 +1,10 @@
 
-# These functions will only load modules in the subshell created.
-# They will not load modules into the parent shell.
-#
-# So make sure any necessary modules are loaded before submitting
-# jobs to the cluster!
-"""
-import sys, subprocess
-
-def module_loaded(module):
-    output = subprocess.check_output("module list", shell=True, executable="/bin/bash")
-    return module not in output:
-
-def load_module(module):
-    try:
-        subprocess.check_call("module load {}".format(module), shell=True, executable="/bin/bash")
-
-    except subprocess.CalledProcessError as e:
-        print("Failed to load {}: {}".format(module, e), file=sys.stderr)
-        print("Checking that {} is loaded:".format(module), file=sys.stderr)
-
-    finally:
-        if not module_loaded(module):
-        	raise RuntimeError("The module {} is not loaded".format(module))
-"""
-
 def load(args):
+    """
+    Used to load the correct cluster module, from a list of arguments which
+    may have the cluster name in it.
+    """
+    
     import pkgutil
     import data.cluster as clusters
 
