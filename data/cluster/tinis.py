76caebdb7e2efac171355db998b0b66f66dd3e14
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-02-02 16:01:44 +0000

    Update the tinis submit script

--- data/cluster/tinis.py
+++ data/cluster/tinis.py
@@ -5,6 +5,8 @@ def name():
 
 def type():
     return "pbs"
+    # Also MOAB
+    # Also SLURM
 
 def url():
     return "tinis.csc.warwick.ac.uk"
@@ -28,6 +30,11 @@ def copy_to():
     subprocess.check_call("rsync -avz -e \"ssh -i {0}/.ssh/id_rsa\" cluster {1}@{2}:~/slp-algorithms-tinyos".format(
         os.environ['HOME'], username, url()), shell=True)
 
+def copy_safety_periods():
+    username = raw_input("Enter your {} username: ".format(name().title()))
+    subprocess.check_call("rsync -avz --rsync-path=\"mkdir -p ~/slp-algorithms-tinyos/results/protectionless && rsync\" -e ssh results/protectionless/protectionless-results.csv {0}@{1}:~/slp-algorithms-tinyos/results/protectionless/protectionless-results.csv".format(
+        username, url()), shell=True)
+
 def copy_back(dirname):
     username = raw_input("Enter your {} username: ".format(name().title()))
     subprocess.check_call("rsync -avz -e \"ssh -i {0}/.ssh/id_rsa\" {1}@{2}:~/slp-algorithms-tinyos/cluster/{3}/*.txt results/{3}".format(
@@ -41,11 +48,11 @@ def submitter(notify_emails=None):
     jobs = ppn()
     ram_per_job_mb = int(math.floor(((ram_per_node() * ppn()) - ram_for_os_mb) / jobs))
 
-    cluster_command = "qsub -j oe -h -l nodes=1:ppn={} -l walltime=24:00:00 -l mem={}mb -N \"{{}}\"".format(ppn(), ppn() * ram_per_job_mb)
+    cluster_command = "msub -j oe -h -q cnode -l nodes=1:ppn={} -l walltime=24:00:00 -l mem={}mb -N \"{{}}\"".format(ppn(), ppn() * ram_per_job_mb)
 
     if notify_emails is not None and len(notify_emails) > 0:
         cluster_command += " -m ae -M {}".format(",".join(notify_emails))
 
-    prepare_command = "cd $PBS_O_WORKDIR"
+    prepare_command = ""
 
     return Submitter(cluster_command, prepare_command, ppn() * threads_per_processor())
