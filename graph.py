c40a4bc85f2fa1e6b1364ed25d5d8ac2ddf497cf
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-03-10 16:22:22 +0000

    Add absolute error heapmaps for the different angle prediction heatmaps

--- graph.py
+++ graph.py
@@ -100,84 +100,63 @@ def cosine_rule(opp, adj, hyp):
 	except ZeroDivisionError:
 		return float('inf')
 
-if args.source_angle_estimate_heatmap:
-	def angle_to(conf, n, i):
-		ssd = float(conf.ssd(i))
-		dsink = float(conf.node_sink_distance(n))
-		dsrc = float(conf.node_source_distance(n, i))
 
-		return cosine_rule(ssd, dsrc, dsink)
+def angle_to_estimate_hops(conf, n, i):
+	ssd = float(conf.ssd(i))
+	dsink = float(conf.node_sink_distance(n))
+	dsrc = float(conf.node_source_distance(n, i))
 
-	def angle_between(conf, n, i, j):
-		ssd_i = conf.ssd(i)
-		ssd_j = conf.ssd(j)
+	return cosine_rule(ssd, dsrc, dsink)
 
-		dsink = conf.node_sink_distance(n)
+def angle_between_estimate_hops(conf, n, i, j):
+	ssd_i = conf.ssd(i)
+	ssd_j = conf.ssd(j)
 
-		dsrc_i = conf.node_source_distance(n, i)
-		dsrc_j = conf.node_source_distance(n, j)
+	dsink = conf.node_sink_distance(n)
 
-		angle_i = angle_to(conf, n, i)
-		angle_j = angle_to(conf, n, j)
+	dsrc_i = conf.node_source_distance(n, i)
+	dsrc_j = conf.node_source_distance(n, j)
 
-		circle_angle = 2.0 * math.pi - angle_i - angle_j
-		add_angle = angle_i + angle_j
-		subtract_angle = abs(angle_i - angle_j)
+	angle_i = angle_to_estimate_hops(conf, n, i)
+	angle_j = angle_to_estimate_hops(conf, n, j)
 
-		try:
-			return min([angle for angle in [circle_angle, add_angle, subtract_angle] if is_valid_angle(angle)])
-		except ValueError:
-			return 0
+	circle_angle = 2.0 * math.pi - angle_i - angle_j
+	add_angle = angle_i + angle_j
+	subtract_angle = abs(angle_i - angle_j)
 
-	def zextractor(configuration, nid):
-		(a, b) = tuple(configuration.source_ids)
-		angle = angle_between(configuration, nid, a, b) * (180.0 / math.pi)
-		return '?' if np.isnan(angle) else angle
-
-	angle_heapmaps("source_angle_estimate", zextractor)
-
-
-
-
-if args.source_angle_meters_heatmap:
-	def angle_to_meters(conf, n, i):
-		ssd = conf.ssd_meters(i)
-		dsink = conf.node_sink_distance_meters(n)
-		dsrc = conf.node_source_distance_meters(n, i)
-
-		return cosine_rule(ssd, dsrc, dsink)
-
-	def angle_between_meters(conf, n, i, j):
-		ssd_i = conf.ssd_meters(i)
-		ssd_j = conf.ssd_meters(j)
-
-		dsink = conf.node_sink_distance_meters(n)
-
-		dsrc_i = conf.node_source_distance_meters(n, i)
-		dsrc_j = conf.node_source_distance_meters(n, j)
-
-		angle_i = angle_to_meters(conf, n, i)
-		angle_j = angle_to_meters(conf, n, j)
-
-		circle_angle = 2.0 * math.pi - angle_i - angle_j
-		add_angle = angle_i + angle_j
-		subtract_angle = abs(angle_i - angle_j)
-
+	try:
 		return min([angle for angle in [circle_angle, add_angle, subtract_angle] if is_valid_angle(angle)])
-
-	def zextractor(configuration, nid):
-		if nid in configuration.source_ids or nid == configuration.sink_id:
-			return '?'
-
-		(a, b) = tuple(configuration.source_ids)
-		angle = angle_between_meters(configuration, nid, a, b) * (180.0 / math.pi)
-		return '?' if np.isnan(angle) else angle
-
-	angle_heapmaps("source_angle_meters", zextractor)
+	except ValueError:
+		return 0
 
 
-if args.source_angle_det_heatmap:
-	def angle_between_det(conf, n, i, j):
+def angle_to_estimate_meters(conf, n, i):
+	ssd = conf.ssd_meters(i)
+	dsink = conf.node_sink_distance_meters(n)
+	dsrc = conf.node_source_distance_meters(n, i)
+
+	return cosine_rule(ssd, dsrc, dsink)
+
+def angle_between_estimate_meters(conf, n, i, j):
+	ssd_i = conf.ssd_meters(i)
+	ssd_j = conf.ssd_meters(j)
+
+	dsink = conf.node_sink_distance_meters(n)
+
+	dsrc_i = conf.node_source_distance_meters(n, i)
+	dsrc_j = conf.node_source_distance_meters(n, j)
+
+	angle_i = angle_to_meters(conf, n, i)
+	angle_j = angle_to_meters(conf, n, j)
+
+	circle_angle = 2.0 * math.pi - angle_i - angle_j
+	add_angle = angle_i + angle_j
+	subtract_angle = abs(angle_i - angle_j)
+
+	return min([angle for angle in [circle_angle, add_angle, subtract_angle] if is_valid_angle(angle)])
+
+
+def angle_between_det_meters(conf, n, i, j):
 
 		dsrc_i = conf.node_source_distance_meters(n, i)
 		dsrc_j = conf.node_source_distance_meters(n, j)
@@ -199,7 +178,6 @@ if args.source_angle_det_heatmap:
 		d2 = None if c == 0 else sqrt((part1 + (rsqrt1 * rsqrt2).real + part2) / c**2) / sqrt(2)
 
 		d3 = None if c == 0 or (a**2 - e**2) * (b**2 - f**2) == 0 else sqrt(a**4 * (- f**2) + a**2 * b**2 * e**2 + a**2 * b**2 * f**2 + a**2 * e**2 * f**2 - a**2 * f**4 - b**4 * e**2 - b**2 * e**4 + b**2 * e**2 * f**2) / sqrt((a**2 - e**2) * (b**2 - f**2))
-
 
 		print(n, i, j, conf.node_source_distance_meters(j, i), d1, d2, d3)
 
@@ -224,34 +202,88 @@ if args.source_angle_det_heatmap:
 
 		return 0 if len(angles) == 0 else min(angles)
 
+def angle_between_actual_meters(conf, n, i, j):
+	dsrc_i = conf.node_source_distance_meters(n, i)
+	dsrc_j = conf.node_source_distance_meters(n, j)
+
+	dist_i_j = conf.node_source_distance_meters(i, j)
+
+	return cosine_rule(dist_i_j, dsrc_i, dsrc_j)
+
+if args.source_angle_estimate_heatmap:
+	def zextractor(configuration, nid):
+		(a, b) = tuple(configuration.source_ids)
+		angle = angle_between_estimate_hops(configuration, nid, a, b) * (180.0 / math.pi)
+		return '?' if np.isnan(angle) else angle
+
+	def zextractor_error(configuration, nid):
+		if nid in configuration.source_ids or nid == configuration.sink_id:
+			return '?'
+
+		(a, b) = tuple(configuration.source_ids)
+		angle1 = angle_between_estimate_hops(configuration, nid, a, b) * (180.0 / math.pi)
+		angle2 = angle_between_actual_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		return '?' if np.isnan(angle1) or np.isnan(angle2) else abs(angle1 - angle2)
+
+	angle_heapmaps("source_angle_estimate", zextractor)
+	angle_heapmaps("source_angle_estimate_error", zextractor_error)
+
+
+
+
+if args.source_angle_meters_heatmap:
 	def zextractor(configuration, nid):
 		if nid in configuration.source_ids or nid == configuration.sink_id:
 			return '?'
 
 		(a, b) = tuple(configuration.source_ids)
-		angle = angle_between_det(configuration, nid, a, b) * (180.0 / math.pi)
+		angle = angle_between_estimate_meters(configuration, nid, a, b) * (180.0 / math.pi)
 		return '?' if np.isnan(angle) else angle
 
+	def zextractor_error(configuration, nid):
+		if nid in configuration.source_ids or nid == configuration.sink_id:
+			return '?'
+
+		(a, b) = tuple(configuration.source_ids)
+		angle1 = angle_between_estimate_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		angle2 = angle_between_actual_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		return '?' if np.isnan(angle1) or np.isnan(angle2) else abs(angle1 - angle2)
+
+	angle_heapmaps("source_angle_meters", zextractor)
+	angle_heapmaps("source_angle_meters_error", zextractor_error)
+
+
+if args.source_angle_det_heatmap:
+	def zextractor(configuration, nid):
+		if nid in configuration.source_ids or nid == configuration.sink_id:
+			return '?'
+
+		(a, b) = tuple(configuration.source_ids)
+		angle = angle_between_det_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		return '?' if np.isnan(angle) else angle
+
+	def zextractor_error(configuration, nid):
+		if nid in configuration.source_ids or nid == configuration.sink_id:
+			return '?'
+
+		(a, b) = tuple(configuration.source_ids)
+		angle1 = angle_between_det_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		angle2 = angle_between_actual_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		return '?' if np.isnan(angle1) or np.isnan(angle2) else abs(angle1 - angle2)
+
 	angle_heapmaps("source_angle_det", zextractor)
+	angle_heapmaps("source_angle_det_error", zextractor_error)
 
 
 
 
 if args.source_angle_actual_heatmap:
-	def angle_between_actual(conf, n, i, j):
-		dsrc_i = conf.node_source_distance_meters(n, i)
-		dsrc_j = conf.node_source_distance_meters(n, j)
-
-		dist_i_j = conf.node_source_distance_meters(i, j)
-
-		return cosine_rule(dist_i_j, dsrc_i, dsrc_j)
-
 	def zextractor(configuration, nid):
 		if nid in configuration.source_ids or nid == configuration.sink_id:
 			return '?'
 
 		(a, b) = tuple(configuration.source_ids)
-		angle = angle_between_actual(configuration, nid, a, b) * (180.0 / math.pi)
+		angle = angle_between_actual_meters(configuration, nid, a, b) * (180.0 / math.pi)
 		return '?' if np.isnan(angle) else angle
 
 	angle_heapmaps("source_angle_actual", zextractor)
