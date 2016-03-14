3925d1259e88b1fb35744a8f255189dfba112bb3
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-03-14 12:08:37 +0000

    Add atan heatmaps

--- graph.py
+++ graph.py
@@ -18,6 +18,7 @@ parser.add_argument("--source-angle-estimate-heatmap", action='store_true', defa
 parser.add_argument("--source-angle-meters-heatmap", action='store_true', default=False)
 parser.add_argument("--source-angle-actual-heatmap", action='store_true', default=False)
 parser.add_argument("--source-angle-det-heatmap", action='store_true', default=False)
+parser.add_argument("--source-angle-tan-heatmap", action='store_true', default=False)
 
 args = parser.parse_args(sys.argv[1:])
 
@@ -87,7 +88,7 @@ def angle_heapmaps(description, zextractor):
 	summary_grapher.run()
 
 def is_valid_angle(angle):
-	return not np.isinf(angle) and 0 <= angle <= math.pi 
+	return not np.isinf(angle) and not np.isnan(angle) and 0 <= angle <= math.pi 
 
 def cosine_rule(opp, adj, hyp):
 
@@ -157,50 +158,93 @@ def angle_between_estimate_meters(conf, n, i, j):
 
 
 def angle_between_det_meters(conf, n, i, j):
+	dsrc_i = conf.node_source_distance_meters(n, i)
+	dsrc_j = conf.node_source_distance_meters(n, j)
 
-		dsrc_i = conf.node_source_distance_meters(n, i)
-		dsrc_j = conf.node_source_distance_meters(n, j)
+	a = conf.ssd_meters(i)
+	b = conf.ssd_meters(j)
 
-		a = conf.ssd_meters(i)
-		b = conf.ssd_meters(j)
+	c = conf.node_sink_distance_meters(n)
 
-		c = conf.node_sink_distance_meters(n)
+	e = dsrc_i
+	f = dsrc_j
 
-		e = dsrc_i
-		f = dsrc_j
+	part1 = - a**2 * b**2 + a**2 * c**2 + a**2 * f**2
+	rsqrt1 = sqrt(a**4 - (2 * a**2 * c**2) - (2 * a**2 * e**2) + c**4 - (2 * c**2 * e**2) + e**4)
+	rsqrt2 = sqrt(b**4 - (2 * b**2 * c**2) - (2 * b**2 * f**2) + c**4 - (2 * c**2 * f**2) + f**4)
+	part2 = b**2 * c**2 + b**2 * e**2 - c**4 + c**2 * e**2 + c**2 * f**2 - e**2 * f**2
 
-		part1 = - a**2 * b**2 + a**2 * c**2 + a**2 * f**2
-		rsqrt1 = sqrt(a**4 - (2 * a**2 * c**2) - (2 * a**2 * e**2) + c**4 - (2 * c**2 * e**2) + e**4)
-		rsqrt2 = sqrt(b**4 - (2 * b**2 * c**2) - (2 * b**2 * f**2) + c**4 - (2 * c**2 * f**2) + f**4)
-		part2 = b**2 * c**2 + b**2 * e**2 - c**4 + c**2 * e**2 + c**2 * f**2 - e**2 * f**2
+	d1 = None if c == 0 else sqrt((part1 - (rsqrt1 * rsqrt2).real + part2) / c**2) / sqrt(2)
+	d2 = None if c == 0 else sqrt((part1 + (rsqrt1 * rsqrt2).real + part2) / c**2) / sqrt(2)
 
-		d1 = None if c == 0 else sqrt((part1 - (rsqrt1 * rsqrt2).real + part2) / c**2) / sqrt(2)
-		d2 = None if c == 0 else sqrt((part1 + (rsqrt1 * rsqrt2).real + part2) / c**2) / sqrt(2)
+	d3 = None if c == 0 or (a**2 - e**2) * (b**2 - f**2) == 0 else sqrt(a**4 * (- f**2) + a**2 * b**2 * e**2 + a**2 * b**2 * f**2 + a**2 * e**2 * f**2 - a**2 * f**4 - b**4 * e**2 - b**2 * e**4 + b**2 * e**2 * f**2) / sqrt((a**2 - e**2) * (b**2 - f**2))
 
-		d3 = None if c == 0 or (a**2 - e**2) * (b**2 - f**2) == 0 else sqrt(a**4 * (- f**2) + a**2 * b**2 * e**2 + a**2 * b**2 * f**2 + a**2 * e**2 * f**2 - a**2 * f**4 - b**4 * e**2 - b**2 * e**4 + b**2 * e**2 * f**2) / sqrt((a**2 - e**2) * (b**2 - f**2))
+	print(n, i, j, conf.node_source_distance_meters(j, i), d1, d2, d3)
 
-		print(n, i, j, conf.node_source_distance_meters(j, i), d1, d2, d3)
+	distances = [d1, d2, d3]
+	angles = []
 
-		distances = [d1, d2, d3]
-		angles = []
+	for distance in distances:
+		if distance is None:
+			continue
 
-		for distance in distances:
-			if distance is None:
-				continue
+		if distance.imag != 0:
+			continue
 
-			if distance.imag != 0:
-				continue
+		try:
+			angle = cosine_rule(distance.real, dsrc_i, dsrc_j)
+			angles.append(angle)
+		except ValueError:
+			pass
 
-			try:
-				angle = cosine_rule(distance.real, dsrc_i, dsrc_j)
-				angles.append(angle)
-			except ValueError:
-				pass
+	print(angles)
+	print()
 
-		print(angles)
-		print()
+	return 0 if len(angles) == 0 else min(angles)
 
-		return 0 if len(angles) == 0 else min(angles)
+
+def angle_between_tan_meters(conf, n, i, j):
+	dsrc_i = conf.node_source_distance_meters(n, i)
+	dsrc_j = conf.node_source_distance_meters(n, j)
+
+	e = conf.ssd_meters(i)
+	f = conf.ssd_meters(j)
+
+	d = conf.node_sink_distance_meters(n)
+
+	c = dsrc_i
+	g = dsrc_j
+
+	bxp1 = (c**2 + d**2 - e**2) * (d**2 - f**2 + g**2)
+	bxp2 = sqrt((c-d-e)*(c+d-e)*(c-d+e)*(c+d+e)*(d-f-g)*(d+f-g)*(d-f+g)*(d+f+g))
+
+	byp1 = d**2 - f**2 + g**2
+	byp2 = sqrt((c-d-e)*(c+d-e)*(c-d+e)*(c+d+e)*(d-f-g)*(d+f-g)*(d-f+g)*(d+f+g))
+	byp3 = 2 * (d**2+e**2)*(d**2-f**2+g**2)
+	byp4 = (d-e)*(d+e)*(d**2-f**2+g**2)
+	byp5 = 4 * c * d**2 * sqrt(-(c-d-e)*(c+d-e)*(c-d+e)*(c+d+e))
+
+	if byp5 == 0:
+		return float('NaN')
+
+	bx1 = (bxp1 + bxp2) / (4 * c * d**2)
+	by1 = (c**4 * +byp1 + c**2 * (byp2 - byp3) + (d-e)*(d+e)*(byp2 + byp4)) / byp5
+
+	bx2 = (bxp1 - bxp2) / (4 * c * d**2)
+	by2 = (c**4 * -byp1 + c**2 * (byp2 + byp3) + (d-e)*(d+e)*(byp2 - byp4)) / byp5
+
+	print(n, i, j, conf.node_source_distance_meters(j, i), (bx1, by1), (bx2, by2))
+
+	if any(value.imag != 0 for value in [bx1, by1, bx2, by2]):
+		raise RuntimeError("Imag")
+
+	angle1 = math.atan2(by1.real, bx1.real)
+	angle2 = math.atan2(by2.real, bx2.real)
+
+	angles = [angle for angle in [angle1, angle2] if is_valid_angle(angle)]
+
+	return 0 if len(angles) == 0 else min(angles)
+
 
 def angle_between_actual_meters(conf, n, i, j):
 	dsrc_i = conf.node_source_distance_meters(n, i)
@@ -275,6 +319,27 @@ if args.source_angle_det_heatmap:
 	angle_heapmaps("source_angle_det_error", zextractor_error)
 
 
+
+if args.source_angle_tan_heatmap:
+	def zextractor(configuration, nid):
+		if nid in configuration.source_ids or nid == configuration.sink_id:
+			return '?'
+
+		(a, b) = tuple(configuration.source_ids)
+		angle = angle_between_tan_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		return '?' if np.isnan(angle) else angle
+
+	def zextractor_error(configuration, nid):
+		if nid in configuration.source_ids or nid == configuration.sink_id:
+			return '?'
+
+		(a, b) = tuple(configuration.source_ids)
+		angle1 = angle_between_tan_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		angle2 = angle_between_actual_meters(configuration, nid, a, b) * (180.0 / math.pi)
+		return '?' if np.isnan(angle1) or np.isnan(angle2) else abs(angle1 - angle2)
+
+	angle_heapmaps("source_angle_tan", zextractor)
+	angle_heapmaps("source_angle_tan_error", zextractor_error)
 
 
 if args.source_angle_actual_heatmap:
