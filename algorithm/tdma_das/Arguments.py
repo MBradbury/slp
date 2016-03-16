b52b03bd75c2996542e36da7a8fdc20b8eedaf5d
Jack Kirton <jack.d.kirton@gmail.com>
2016-03-16 15:33:12 +0000

    Move parameters to command line

--- algorithm/tdma_das/Arguments.py
+++ algorithm/tdma_das/Arguments.py
@@ -5,12 +5,15 @@ import simulator.MobilityModel
 
 class Arguments(ArgumentsCommon):
     def __init__(self):
-        parser = argparse.ArgumentParser(description="SLP TDMA DAS", add_help=True)
+        parser = argparse.ArgumentParser(description="Protectionless TDMA DAS", add_help=True)
         super(Arguments, self).__init__(parser, has_safety_period=False)
 
         parser.add_argument("--source-period",
             type=simulator.SourcePeriodModel.eval_input, required=True)
-        parser.add_argument("--broadcast-period", type=float, required=True)
+        parser.add_argument("-sp", "--slot-period", type=float, required=True)
+        parser.add_argument("-dp", "--dissem-period", type=float, required=True)
+        parser.add_argument("-ts", "--tdma-num-slots", type=int, required=True)
+        parser.add_argument("-ll", "--slot-loop-length", type=int, required=True)
         parser.add_argument("--source-mobility",
             type=simulator.MobilityModel.eval_input,
             default=simulator.MobilityModel.StationaryMobilityModel())
@@ -18,6 +21,10 @@ class Arguments(ArgumentsCommon):
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
 
-        result["BROADCAST_PERIOD_MS"] = self.args.broadcast_period * 1000
+        result["SLOT_PERIOD_MS"] = int(self.args.slot_period * 1000)
+        result["DISSEM_PERIOD_MS"] = int(self.args.dissem_period * 1000)
+        result["TDMA_NUM_SLOTS"] = self.args.tdma_num_slots
+        result["SLOT_LOOP_LENGTH"] = self.args.slot_loop_length
+
 
         return result
