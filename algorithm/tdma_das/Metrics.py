a7d8d9b8e1f1339a18ee4e37d99c98a6d362638e
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-03-16 16:33:46 +0000

    TDMADAS: Add a metric that record the first time a Normal message is sent

--- algorithm/tdma_das/Metrics.py
+++ algorithm/tdma_das/Metrics.py
@@ -6,9 +6,22 @@ class Metrics(MetricsCommon):
     def __init__(self, sim, configuration):
         super(Metrics, self).__init__(sim, configuration)
 
+    def first_normal_send_times(self):
+    	result = {}
+
+    	for ((node_id, seq_no), time) in self.normal_sent_time.items():
+    		if node_id not in result:
+    			result[node_id] = time
+    		else:
+    			result[node_id] = min(result[node_id], time)
+
+    	return result
+
     @staticmethod
     def items():
         d = MetricsCommon.items()
         d["DissemSent"]               = lambda x: x.number_sent("Dissem")
+
+        d["FirstSendTime"]            = lambda x: x.first_normal_send_times()
 
         return d
