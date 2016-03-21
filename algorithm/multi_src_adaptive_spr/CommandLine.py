a426a1476c99098e29b375caf7d8d6969983a213
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-03-21 08:48:52 +0000

    Remove debug print statement

--- algorithm/multi_src_adaptive_spr/CommandLine.py
+++ algorithm/multi_src_adaptive_spr/CommandLine.py
@@ -90,8 +90,6 @@ class CLI(CommandLineCommon.CLI):
             [self.distance], self.source_periods, self.approaches
         ))
 
-        print(dir(self))
-
         argument_product = self.adjust_source_period_for_multi_source(argument_product)
 
         runner.run(self.executable_path, self.repeats, self.parameter_names(), argument_product, self._time_estimater)
