2b7876e5af9344d68231c2e6e8a42d7768e92f94
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-04-05 13:07:55 +0100

    ProtectionlessCTP: Implement CTP

--- algorithm/protectionless_aggregation/Arguments.py
+++ algorithm/protectionless_aggregation/Arguments.py
@@ -5,7 +5,7 @@ import simulator.MobilityModel
 
 class Arguments(ArgumentsCommon):
     def __init__(self):
-        parser = argparse.ArgumentParser(description="SLP Protectionless Aggregation", add_help=True)
+        parser = argparse.ArgumentParser(description="SLP Protectionless CTP", add_help=True)
         super(Arguments, self).__init__(parser, has_safety_period=False)
 
         parser.add_argument("--source-period",
@@ -14,11 +14,7 @@ class Arguments(ArgumentsCommon):
             type=simulator.MobilityModel.eval_input,
             default=simulator.MobilityModel.StationaryMobilityModel())
 
-        parser.add_argument("--aggregation-period", type=float, required=True)
-
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
-
-        result["AGGREGATION_PERIOD_MS"] = int(self.args.aggregation_period * 1000)
 
         return result
