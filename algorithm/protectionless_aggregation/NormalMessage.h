8ae4e5a6f5cfaa5d9e792961e37641fb6a19a1ad
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-04-05 11:27:46 +0100

    ProtectionlessAggregation: Start work on a basic aggregation algorithm

---
+++ algorithm/protectionless_aggregation/NormalMessage.h
@@ -0,0 +1,22 @@
+#ifndef SLP_MESSAGES_NORMALMESSAGE_H
+#define SLP_MESSAGES_NORMALMESSAGE_H
+
+#include "SequenceNumber.h"
+
+typedef nx_struct NormalMessage {
+  NXSequenceNumber sequence_number_start; // Should be the 
+  NXSequenceNumber sequence_number_end;
+
+  // The number of hops that this message
+  // has travelled from the source. 
+  nx_uint16_t source_distance;
+
+  // The id of the node that sent this message
+  nx_am_addr_t source_id;
+
+} NormalMessage;
+
+inline SequenceNumberWithBottom Normal_get_sequence_number(const NormalMessage* msg) { return msg->sequence_number_start; }
+inline int32_t Normal_get_source_id(const NormalMessage* msg) { return msg->source_id; }
+
+#endif // SLP_MESSAGES_NORMALMESSAGE_H
