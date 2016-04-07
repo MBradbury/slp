3e5f80048d265285f4227545104b3448af576224
Chen Gu <c.gu.warwick@gmail.com>
2016-04-07 17:05:02 +0100

    merge

--- algorithm/phantom_chen/Constants.h
+++ algorithm/phantom_chen/Constants.h
@@ -9,4 +9,16 @@ enum Channels
 #define SLP_MAX_NUM_SINKS 1
 #define SLP_MAX_NUM_SOURCES 20
 
+#define NORTH_WEST_DIRECTION 0
+#define NORTH_EAST_DIRECTION 1
+#define SOUTH_WEST_DIRECTION 2
+#define SOUTH_EAST_DIRECTION 3
+#define BIASED_X_AXIS        4
+#define BIASED_Y_AXIS        5
+
+#define SHORT_RANDOM_MESSAGE 0
+#define LONG_RANDOM_MESSAGE  1
+#define TRUE 1
+#define FALSE 0
+
 #endif // SLP_CONSTANTS_H
