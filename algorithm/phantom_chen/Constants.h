718169b33f2f3614f341e3242a5a525fbbc43165
Chen Gu <c.gu.warwick@warwick.ac.uk>
2016-04-12 15:04:59 +0100

    making wait-before-short works

--- algorithm/phantom_chen/Constants.h
+++ algorithm/phantom_chen/Constants.h
@@ -6,6 +6,8 @@ enum Channels
 	NORMAL_CHANNEL = 1,
 };
 
+#define METRIC_RCV_NORMAL(msg) METRIC_RCV(Normal, source_addr, msg->source_id, msg->sequence_number, msg->source_distance + 1)
+
 #define SLP_MAX_NUM_SINKS 1
 #define SLP_MAX_NUM_SOURCES 20
 
@@ -16,9 +18,14 @@ enum Channels
 #define BIASED_X_AXIS        4
 #define BIASED_Y_AXIS        5
 
+#define SHORT_RANDOM_WALK 0
+#define LONG_RANDOM_WALK 1
+
 //define the global vaiable.
 //make it work for multiple sources.
 uint16_t message_no = 1;
+uint16_t current_message = 0;
+uint16_t previous_message = 0;
 
 #endif // SLP_CONSTANTS_H
 
