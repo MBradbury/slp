24118d3cc8d79a63846854f50af73d788d0c4534
Chen Gu <c.gu.warwick@gmail.com>
2016-05-06 16:59:22 +0100

    merge with matt

--- algorithm/phantom_chen/CommandLine.py
+++ algorithm/phantom_chen/CommandLine.py
@@ -107,7 +107,7 @@ class CLI(CommandLineCommon.CLI):
 
     communication_models = ["ideal"]
 
-    sizes = [11, 15, 21, 25]
+    sizes = [11]
 
     source_periods = [1.0, 0.5, 0.25, 0.125]
 
@@ -116,9 +116,9 @@ class CLI(CommandLineCommon.CLI):
         'Source2CornerTop',
         'Source3CornerTop',
 
-        'SinkCorner',
-        'SinkCorner2Source',
-        'SinkCorner3Source',
+        #'SinkCorner',
+        #'SinkCorner2Source',
+        #'SinkCorner3Source',
 
         #'FurtherSinkCorner',
         #'FurtherSinkCorner2Source',
@@ -140,7 +140,7 @@ class CLI(CommandLineCommon.CLI):
     "ShortLong"
     ]
 
-    wait_before_short = [0, 100, 200]
+    wait_before_short = [100]
 
     short_counts = [1]
     long_counts = [2]
