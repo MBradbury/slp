1a01148f9e1b57a6a05ecbc8978d2be2f8fff0ed
Chen Gu <c.gu.warwick@gmail.com>
2016-06-09 17:20:40 +0100

    phantom_walkabouts: biased random walk now works

--- algorithm/phantom_walkabouts/Constants.h
+++ algorithm/phantom_walkabouts/Constants.h
@@ -11,7 +11,8 @@ enum Channels
 #define SLP_MAX_NUM_SINKS 1
 #define SLP_MAX_NUM_SOURCES 20
 #define SLP_MAX_1_HOP_NEIGHBOURHOOD 16
-#define SLP_MAX_NUM_AWAY_MESSAGES 3
+#define SLP_MAX_NUM_AWAY_MESSAGES 4
 #define CENTRE_AREA 5 		//hops from the real cenre
+#define MAX_NUM_NEIGHBOURS 2 //max neighbours each direction
 
 #endif // SLP_CONSTANTS_H
