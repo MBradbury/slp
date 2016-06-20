bab0fd3e833b063eac66eb390fd3e17b9c41fd24
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-06-20 11:31:26 +0100

    MultiSrcAdaptive: add algorithm

---
+++ algorithm/multi_src_adaptive/Analysis.py
@@ -0,0 +1,68 @@
+
+from data.analysis import AnalyzerCommon
+
+class Analyzer(AnalyzerCommon):
+    def __init__(self, results_directory):
+        super(Analyzer, self).__init__(results_directory, self.results_header(), self.normalised_parameters())
+
+    @staticmethod
+    def normalised_parameters():
+        return (
+            ('Sent', 'TimeTaken'),
+            (('Sent', 'TimeTaken'), 'num_nodes'),
+            ('FakeSent', 'TimeTaken'),
+            (('FakeSent', 'TimeTaken'), 'source_rate'),
+            ('NormalSent', 'TimeTaken'),
+
+            ('energy_impact', 'num_nodes'),
+            (('energy_impact', 'num_nodes'), 'TimeTaken'),
+            ('daily_allowance_used', '1'),
+        )
+
+    @staticmethod
+    def results_header():
+        d = AnalyzerCommon.common_results_header()
+
+        d['approach']           = lambda x: x.opts['approach']
+
+        AnalyzerCommon.common_results(d)
+        
+        d['captured']           = lambda x: str(x.average_of['Captured'])
+        d['attacker moves']     = lambda x: AnalyzerCommon._format_results(x, 'AttackerMoves')
+        d['attacker distance']  = lambda x: AnalyzerCommon._format_results(x, 'AttackerDistance', average_corrector=Analyzer._correct_attacker_distance)
+        d['received ratio']     = lambda x: AnalyzerCommon._format_results(x, 'ReceiveRatio')
+        d['normal latency']     = lambda x: AnalyzerCommon._format_results(x, 'NormalLatency')
+        d['normal']             = lambda x: AnalyzerCommon._format_results(x, 'NormalSent')
+        d['away']               = lambda x: AnalyzerCommon._format_results(x, 'AwaySent')
+        d['choose']             = lambda x: AnalyzerCommon._format_results(x, 'ChooseSent')
+        d['fake']               = lambda x: AnalyzerCommon._format_results(x, 'FakeSent')
+        d['tfs']                = lambda x: AnalyzerCommon._format_results(x, 'TFS')
+        d['pfs']                = lambda x: AnalyzerCommon._format_results(x, 'PFS')
+        d['fake to normal']     = lambda x: AnalyzerCommon._format_results(x, 'FakeToNormal')
+        d['ssd']                = lambda x: AnalyzerCommon._format_results(x, 'NormalSinkSourceHops')
+        
+        d['sent heatmap']       = lambda x: AnalyzerCommon._format_results(x, 'SentHeatMap')
+        d['received heatmap']   = lambda x: AnalyzerCommon._format_results(x, 'ReceivedHeatMap')
+
+        d['norm(sent,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(Sent,TimeTaken)')
+        d['norm(norm(sent,time taken),num_nodes)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(Sent,TimeTaken),num_nodes)')
+       
+        d['norm(fake,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(FakeSent,TimeTaken)')
+        d['norm(norm(fake,time taken),source rate)'] = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(FakeSent,TimeTaken),source_rate)')
+
+        d['norm(normal,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(NormalSent,TimeTaken)')
+
+        d['energy impact per node']   = lambda x: AnalyzerCommon._format_results(x, 'norm(energy_impact,num_nodes)')
+        d['energy impact per node per second']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(energy_impact,num_nodes),TimeTaken)')
+        d['energy allowance used'] = lambda x: AnalyzerCommon._format_results(x, 'norm(daily_allowance_used,1)')
+
+        return d
+
+    @staticmethod
+    def _correct_attacker_distance(x):
+        """The format was changed to have a pair as the key,
+        this allows for multiple attackers and multiple sources."""
+        if isinstance(x, dict) and 0 in x:
+            return {(0, 0): x[0]}
+        else:
+            return x
---
+++ algorithm/multi_src_adaptive/CommandLine.py
@@ -0,0 +1,324 @@
+from __future__ import print_function
+
+import os.path, itertools
+
+from simulator import CommandLineCommon
+
+import algorithm.protectionless as protectionless
+
+# The import statement doesn't work, so we need to use __import__ instead
+template = __import__("algorithm.template", globals(), locals(), ['object'], -1)
+
+from data import results
+
+from data.table import safety_period, fake_result, comparison
+from data.graph import summary, versus, bar, min_max_versus
+from data.util import useful_log10, scalar_extractor
+
+from data.run.common import RunSimulationsCommon as RunSimulations
+
+class CLI(CommandLineCommon.CLI):
+
+    executable_path = 'run.py'
+
+    distance = 4.5
+
+    noise_models = ["meyer-heavy", "casino-lab"]
+
+    communication_models = ["low-asymmetry"]
+
+    sizes = [11, 15, 21, 25]
+
+    source_periods = [1.0, 0.5, 0.25, 0.125]
+
+    configurations = [
+        #'SourceCorner',
+        #'SinkCorner',
+        #'FurtherSinkCorner',
+        #'Generic1',
+        #'Generic2',
+
+        #'RingTop',
+        #'RingOpposite',
+        #'RingMiddle',
+
+        'Source2Corners',
+        #'Source4Corners',
+        'Source2Edges',
+        #'Source4Edges',
+        'Source2Corner',
+        'SourceEdgeCorner',
+
+        #'CircleEdges',
+        #'CircleSourceCentre',
+        #'CircleSinkCentre',
+    ]
+
+    attacker_models = ['SeqNosReactiveAttacker()']
+
+    approaches = ["PB_SINK_APPROACH", "PB_ATTACKER_EST_APPROACH"]
+
+    repeats = 300
+
+    local_parameter_names = ('approach',)
+
+    def __init__(self):
+        super(CLI, self).__init__(__package__)
+
+    def _execute_runner(self, driver, result_path, skip_completed_simulations=True):
+        safety_period_table_generator = safety_period.TableGenerator(protectionless.result_file_path)
+        safety_periods = safety_period_table_generator.safety_periods()
+
+        runner = RunSimulations(
+            driver, self.algorithm_module, result_path,
+            skip_completed_simulations=skip_completed_simulations, safety_periods=safety_periods)
+
+        argument_product = list(itertools.product(
+            self.sizes, self.configurations,
+            self.attacker_models, self.noise_models, self.communication_models,
+            [self.distance], self.source_periods, self.approaches
+        ))
+
+
+        runner.run(self.executable_path, self.repeats, self.parameter_names(), argument_product, self._time_estimater)
+
+
+    def _run_table(self, args):
+        adaptive_results = results.Results(
+            self.algorithm_module.result_file_path,
+            parameters=self.local_parameter_names,
+            results=('normal latency', 'ssd', 'attacker distance'))
+
+        result_table = fake_result.ResultTable(adaptive_results)
+
+        self._create_table(self.algorithm_module.name + "-results", result_table)
+
+    def _run_graph(self, args):
+        graph_parameters = {
+            'normal latency': ('Normal Message Latency (seconds)', 'left top'),
+            'ssd': ('Sink-Source Distance (hops)', 'left top'),
+            'captured': ('Capture Ratio (%)', 'left top'),
+            'fake': ('Fake Messages Sent', 'left top'),
+            'sent': ('Total Messages Sent', 'left top'),
+            'received ratio': ('Receive Ratio (%)', 'left bottom'),
+            'tfs': ('Number of TFS Created', 'left top'),
+            'pfs': ('Number of PFS Created', 'left top'),
+            'attacker distance': ('Meters', 'left top'),
+        }
+
+        adaptive_results = results.Results(
+            self.algorithm_module.result_file_path,
+            parameters=self.local_parameter_names,
+            results=tuple(graph_parameters.keys()))
+
+        for (vary, vary_prefix) in [("source period", " seconds"), ("communication model", "~")]:
+            for (yaxis, (yaxis_label, key_position)) in graph_parameters.items():
+                name = '{}-v-{}'.format(yaxis.replace(" ", "_"), vary.replace(" ", "-"))
+
+                yextractor = lambda x: scalar_extractor(x.get((0, 0), None)) if yaxis == 'attacker distance' else scalar_extractor(x)
+
+                g = versus.Grapher(
+                    self.algorithm_module.graphs_path, name,
+                    xaxis='network size', yaxis=yaxis, vary=vary,
+                    yextractor=yextractor)
+
+                g.xaxis_label = 'Network Size'
+                g.yaxis_label = yaxis_label
+                g.vary_label = vary.title()
+                g.vary_prefix = vary_prefix
+                g.key_position = key_position
+
+                g.create(adaptive_results)
+
+                summary.GraphSummary(
+                    os.path.join(self.algorithm_module.graphs_path, name),
+                    '{}-{}'.format(self.algorithm_module.name, name)
+                ).run()
+
+
+    def _run_comparison_table(self, args):
+        results_to_compare = ('normal latency', 'ssd', 'captured',
+                              'fake', 'received ratio', 'tfs', 'pfs')
+
+        adaptive_results = results.Results(
+            self.algorithm_module.result_file_path,
+            parameters=self.local_parameter_names,
+            results=results_to_compare)
+
+        template_results = results.Results(
+            template.result_file_path,
+            parameters=('fake period', 'temp fake duration', 'pr(tfs)', 'pr(pfs)'),
+            results=results_to_compare)
+
+        result_table = comparison.ResultTable(template_results, adaptive_results)
+
+        self._create_table("adaptive-template-comparison", result_table,
+                           lambda (fp, dur, ptfs, ppfs): ptfs not in {0.2, 0.3, 0.4})
+
+        self._create_table("adaptive-template-comparison-low-prob", result_table,
+                           lambda (fp, dur, ptfs, ppfs): ptfs in {0.2, 0.3, 0.4})
+
+    def _run_comparison_graph(self, args):
+        results_to_compare = ('normal latency', 'ssd', 'captured', 'sent', 'received',
+                              'normal', 'fake', 'away', 'choose', 'received ratio',
+                              'tfs', 'pfs')
+
+        adaptive_results = results.Results(
+            self.algorithm_module.result_file_path,
+            parameters=self.local_parameter_names,
+            results=results_to_compare)
+
+        template_results = results.Results(
+            template.result_file_path,
+            parameters=('fake period', 'temp fake duration', 'pr(tfs)', 'pr(pfs)'),
+            results=results_to_compare)
+
+        result_table = comparison.ResultTable(template_results, adaptive_results)
+
+        def create_comp_bar(show, pc=False):
+            name = 'template-comp-{}-{}'.format(show, "pcdiff" if pc else "diff")
+
+            bar.DiffGrapher(
+                self.algorithm_module.graphs_path, result_table, name,
+                shows=[show],
+                extractor=lambda (diff, pcdiff): pcdiff if pc else diff
+            ).create()
+
+            summary.GraphSummary(
+                os.path.join(self.algorithm_module.graphs_path, name),
+                '{}-{}'.format(self.algorithm_module.name, name).replace(" ", "_")
+            ).run()
+
+        for result_name in results_to_compare:
+            create_comp_bar(result_name, pc=True)
+            create_comp_bar(result_name, pc=False)
+
+        def create_comp_bar_pcdiff(pc=True, modified=lambda x: x, name_addition=None, shows=results_to_compare):
+            name = 'template-comp-{}'.format("pcdiff" if pc else "diff")
+            if name_addition is not None:
+                name += '-{}'.format(name_addition)
+
+            # Normalise wrt to the number of nodes in the network
+            def normalisor(key_names, key_values, params, yvalue):
+                size = key_values[ key_names.index('size') ]
+                result = yvalue / (size * size)
+
+                return modified(result)
+
+            g = bar.DiffGrapher(
+                self.algorithm_module.graphs_path, result_table, name,
+                shows=shows,
+                extractor=lambda (diff, pcdiff): pcdiff if pc else diff,
+                normalisor=normalisor)
+
+            g.yaxis_label = 'Percentage Difference per Node' if pc else 'Average Difference per Node'
+            if name_addition is not None:
+                g.yaxis_label += ' ({})'.format(name_addition)
+
+            g.xaxis_label = 'Parameters (P_{TFS}, D_{TFS}, Pr(TFS), Pr(PFS))'
+
+            g.create()
+
+            summary.GraphSummary(
+                os.path.join(self.algorithm_module.graphs_path, name),
+                '{}-{}'.format(self.algorithm_module.name, name).replace(" ", "_")
+            ).run()
+
+        results_to_show = ('normal', 'fake', 'away', 'choose')
+
+        create_comp_bar_pcdiff(pc=True,  shows=results_to_show)
+        create_comp_bar_pcdiff(pc=False, shows=results_to_show)
+        create_comp_bar_pcdiff(pc=True,  shows=results_to_show, modified=useful_log10, name_addition='log10')
+
+    def _run_min_max_versus(self, args):
+        graph_parameters = {
+            'normal latency': ('Normal Message Latency (seconds)', 'left top'),
+            'ssd': ('Sink-Source Distance (hops)', 'left top'),
+            'captured': ('Capture Ratio (%)', 'right top'),
+            'fake': ('Fake Messages Sent', 'left top'),
+            'sent': ('Total Messages Sent', 'left top'),
+            'received ratio': ('Receive Ratio (%)', 'left bottom'),
+            'tfs': ('Number of TFS Created', 'left top'),
+            'pfs': ('Number of PFS Created', 'left top'),
+        }
+
+        custom_yaxis_range_max = {
+            'fake': 600000,
+            'captured': 20
+        }
+
+        adaptive_results = results.Results(
+            self.algorithm_module.result_file_path,
+            parameters=self.local_parameter_names,
+            results=graph_parameters.keys())
+
+        template_results = results.Results(
+            template.result_file_path,
+            parameters=template.CommandLine.CLI.local_parameter_names,
+            results=graph_parameters.keys())
+
+        def graph_min_max_versus(result_name):
+            name = 'min-max-{}-versus-{}'.format(template.name, result_name)
+
+            g = min_max_versus.Grapher(
+                self.algorithm_module.graphs_path, name,
+                xaxis='network size', yaxis=result_name, vary='approach', yextractor=scalar_extractor)
+
+            g.xaxis_label = 'Network Size'
+            g.yaxis_label = graph_parameters[result_name][0]
+            g.key_position = graph_parameters[result_name][1]
+
+            g.yaxis_font = g.xaxis_font = "',15'"
+
+            g.nokey = True
+            #g.key_font = "',20'"
+            #g.key_spacing = "2"
+            #g.key_width = "-5.5"
+
+            g.point_size = '2'
+            g.line_width = 2
+
+            g.min_label = 'Static - Lowest'
+            g.max_label = 'Static - Highest'
+            g.comparison_label = 'Dynamic'
+            g.vary_label = ''
+
+            if result_name in custom_yaxis_range_max:
+                g.yaxis_range_max = custom_yaxis_range_max[result_name]
+
+            def vvalue_converter(name):
+                return {
+                    'PB_SINK_APPROACH': 'Pull Sink',
+                    'PB_ATTACKER_EST_APPROACH': 'Pull Attacker'
+                }[name]
+            g.vvalue_label_converter = vvalue_converter
+
+            g.generate_legend_graph = True
+
+            g.create(template_results, adaptive_results)
+
+            summary.GraphSummary(
+                os.path.join(self.algorithm_module.graphs_path, name),
+                '{}-{}'.format(self.algorithm_module.name, name).replace(" ", "_")
+            ).run()
+
+        for result_name in graph_parameters.keys():
+            graph_min_max_versus(result_name)
+
+    def run(self, args):
+        super(CLI, self).run(args)
+
+        if 'table' in args:
+            self._run_table(args)
+
+        if 'graph' in args:
+            self._run_graph(args)
+
+        if 'comparison-table' in args:
+            self._run_comparison_table(args)
+
+        if 'comparison-graph' in args:
+            self._run_comparison_graph(args)
+
+        if 'min-max-versus' in args:
+            self._run_min_max_versus(args)
