4d10eed3fb986a592ef25a72cbfaa2f92918f525
Chen Gu <c.gu.warwick@gmail.com>
2016-06-21 16:58:49 +0100

    phnatom_walkabouts: bug fixes for bad seed: -1480174919

--- algorithm/phantom_walkabouts/BeaconMessage.h
+++ algorithm/phantom_walkabouts/BeaconMessage.h
@@ -5,6 +5,8 @@ typedef nx_struct BeaconMessage
 {
   nx_int16_t landmark_distance_of_bottom_left_sender;
   nx_int16_t landmark_distance_of_bottom_right_sender;
+  nx_int16_t landmark_distance_of_top_right_sender;
+  
   nx_int16_t landmark_distance_of_sink_sender;
 
 } BeaconMessage;
