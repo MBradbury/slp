761d2109fe2188b60053f911cecbd6b303f3ece9
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-07-07 09:49:35 +0100

    Update flash_mote.sh to change node id before burning

--- scripts/flash_mote.sh
+++ scripts/flash_mote.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-tos-bsl --telosb -c /dev/ttyUSB0 -r -e -I -p main.ihex
+
+MAIN_IHEX=$1
+NODEID=$2
+
+NEW_MAIN_IHEX="$(MAIN_IHEX)-$(NODEID)"
+
+AMADDR=ActiveMessageAddressC__addr
+
+tos-set-symbols --objcopy msp430-objcopy --objdump msp430-objdump --target ihex $(MAIN_IHEX) $(NEW_MAIN_IHEX) TOS_NODE_ID=$(NODEID) $(AMADDR)=$(NODEID)
+
+tos-bsl --telosb -c /dev/ttyUSB0 -r -e -I -p $(NEW_MAIN_IHEX)
