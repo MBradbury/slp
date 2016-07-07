cad70aa43a0bf732cd6f6adb064ed19256805f20
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-07-07 09:51:29 +0100

    Fix bash variable names

--- scripts/flash_mote.sh
+++ scripts/flash_mote.sh
@@ -3,10 +3,10 @@
 MAIN_IHEX=$1
 NODEID=$2
 
-NEW_MAIN_IHEX="$(MAIN_IHEX)-$(NODEID)"
+NEW_MAIN_IHEX="$MAIN_IHEX-$NODEID"
 
 AMADDR=ActiveMessageAddressC__addr
 
-tos-set-symbols --objcopy msp430-objcopy --objdump msp430-objdump --target ihex $(MAIN_IHEX) $(NEW_MAIN_IHEX) TOS_NODE_ID=$(NODEID) $(AMADDR)=$(NODEID)
+tos-set-symbols --objcopy msp430-objcopy --objdump msp430-objdump --target ihex $MAIN_IHEX $NEW_MAIN_IHEX TOS_NODE_ID=$NODEID $AMADDR=$NODEID
 
-tos-bsl --telosb -c /dev/ttyUSB0 -r -e -I -p $(NEW_MAIN_IHEX)
+tos-bsl --telosb -c /dev/ttyUSB0 -r -e -I -p $NEW_MAIN_IHEX
