8dd013d3e8ca3f892815b91471c6bcd125191472
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-07-28 15:31:54 +0100

    Some estimates for the coordinates of the DCSWarwick topology

--- simulator/Topology.py
+++ simulator/Topology.py
@@ -180,22 +180,31 @@ class DCSWarwick(Topology):
     def __init__(self, size, distance, initial_position=10.0):
         super(DCSWarwick, self).__init__()
 
+        # Distance acts a bit like a range
         self.distance = distance
 
+        floor_distance = 20.0
+
         self.nodes = [
-            np.array((-1, -1), dtype=np.float64), # Padding Node
+            np.array((-100, -100), dtype=np.float64), # Padding Node - There is no node 0 in this network
 
-            np.array((0, 0), dtype=np.float64), # CS2.01
-            np.array((0, 0), dtype=np.float64), # CS2.08 (window)
-            np.array((0, 0), dtype=np.float64), # CS2.08 (shelf)
-            np.array((0, 0), dtype=np.float64), # CS2.06
-            np.array((0, 0), dtype=np.float64), # CS2.02
-            np.array((0, 0), dtype=np.float64), # CS1.02 (far end)
+            np.array((floor_distance*2 + 0, 0),   dtype=np.float64), # CS2.01
+            np.array((floor_distance*2 + 5, 7),   dtype=np.float64), # CS2.08 (window)
+            np.array((floor_distance*2 + 5, 10),  dtype=np.float64), # CS2.08 (shelf)
+            np.array((floor_distance*2 + 5, 5),   dtype=np.float64), # CS2.06
+            np.array((floor_distance*2 + 5, 0),   dtype=np.float64), # CS2.02
 
-            np.array((-1, -1), dtype=np.float64), # Padding Node
-            np.array((-1, -1), dtype=np.float64), # Padding Node
-            np.array((-1, -1), dtype=np.float64), # Padding Node
+            np.array((floor_distance*1 + 5, 5), dtype=np.float64), # CS1.02 (far end)
+            np.array((floor_distance*1 + 5, 10), dtype=np.float64), # CS1.02 (door)
+
+            #np.array((-1, -1), dtype=np.float64), # Padding Node
+            #np.array((-1, -1), dtype=np.float64), # Padding Node
+            #np.array((-1, -1), dtype=np.float64), # Padding Node
         ]
+
+        # Apply the initial position
+        for node in self.nodes:
+            node += initial_position
 
     def __str__(self):
         return "DCSWarwick<>"
