a6c3bc4b891e79dea368329fcf68c67ef11a1ea1
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-07-28 13:36:28 +0100

    Add a way to process logs offline

--- offline.py
+++ offline.py
@@ -2,11 +2,13 @@
 from __future__ import print_function
 
 import argparse
+import copy
 import importlib
 import sys
 
 import simulator.Attacker as Attacker
 import simulator.Configuration as Configuration
+from simulator.Simulation import OfflineSimulation
 
 parser = argparse.ArgumentParser(description="Offline processor", add_help=True)
 
@@ -22,17 +24,31 @@ subparser.add_argument("--algorithm", type=str, required=True)
 subparser.add_argument("-am", "--attacker-model", type=Attacker.eval_input, required=True)
 subparser.add_argument("-c", "--configuration", type=str, required=True, choices=Configuration.names())
 subparser.add_argument("-safety", "--safety-period", type=float, required=True)
+subparser.add_argument("--seed", type=int, required=False)
 
 args = parser.parse_args(sys.argv[1:])
 
 if args.action == "visualise":
-	pass
+    pass
 
 elif args.action == "analyse":
-	Metrics = importlib.import_module("algorithm.{}.Metrics".format(args.algorithm))
+    
+    configuration = Configuration.create_specific(args.configuration, None, 4.5)
 
-	with open(args.merged_log, 'r'):
-		pass
+    with OfflineSimulation("algorithm." + args.algorithm, configuration, args, args.merged_log) as sim:
+
+        # Create a copy of the provided attacker model
+        attacker = copy.deepcopy(args.attacker_model)
+
+        # Setup each attacker model
+        attacker.setup(sim, configuration.sink_id, 0)
+
+        sim.add_attacker(attacker)
+
+        sim.run()
+
+        sim.metrics.print_results()
+
 
 else:
-	raise RuntimeError("Unknown action {}".format(args.action))
+    raise RuntimeError("Unknown action {}".format(args.action))
