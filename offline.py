af487aaf6c65bd170d2468cd22f7c09202e8d6c6
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-07-28 11:55:01 +0100

    Add offline processor

---
+++ offline.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Offline processor", add_help=True)
+
+subparsers = parser.add_subparsers(dest='action')
+
+subparser = subparsers.add_parser("visualise")
+subparser.add_argument("--merged-log", type=str, required=True)
+
+subparser = subparsers.add_parser("analyse")
+subparser.add_argument("--merged-log", type=str, required=True)
+
+args = parser.parse_args(sys.argv[1:])
+
+if args.action == "visualise":
+
+elif args.action == "analyse":
+
+else:
+	raise RuntimeError("Unknown action {}".format(args.action))
