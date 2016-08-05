b89f0505ee0b3593e0c5182cb1df86fa4fa32264
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-08-05 11:36:25 +0100

    Support for building the requirements for running on Indriya

--- simulator/Builder.py
+++ simulator/Builder.py
@@ -3,11 +3,22 @@ import sys
 
 ALLOWED_PLATFORMS = ("micaz", "telosb")
 
-def build_sim(directory, **kwargs):
+def build_sim(directory, platform="micaz", **kwargs):
+
+    if platform not in ALLOWED_PLATFORMS:
+        raise RuntimeError("Unknown build platform {}. Only {} are allowed.".format(platform, ALLOWED_PLATFORMS))
 
     flags = " ".join("-D{}={}".format(k, repr(v)) for (k, v) in kwargs.items())
 
-    command = 'make micaz sim SLP_PARAMETER_CFLAGS="{}"'.format(flags)
+    make_options = {
+        "SLP_PARAMETER_CFLAGS": flags
+    }
+
+    make_options_string = " ".join('{}={}'.format(k, repr(v)) for (k, v) in make_options.items())
+
+    command = 'make {} sim {}'.format(platform, make_options_string)
+
+    print(command)
 
     result = subprocess.check_call(
         command,
@@ -19,14 +30,28 @@ def build_sim(directory, **kwargs):
 
     return result
 
-def build_actual(directory, kind, **kwargs):
+def build_actual(directory, platform, **kwargs):
 
-    if kind not in ALLOWED_PLATFORMS:
-        raise RuntimeError("Unknown build platform {}. Only {} are allowed.".format(kind, ALLOWED_PLATFORMS))
+    if platform not in ALLOWED_PLATFORMS:
+        raise RuntimeError("Unknown build platform {}. Only {} are allowed.".format(platform, ALLOWED_PLATFORMS))
 
     flags = " ".join("-D{}={}".format(k, repr(v)) for (k, v) in kwargs.items())
 
-    command = 'make {} SLP_PARAMETER_CFLAGS="{}" USE_SERIAL_PRINTF=1'.format(kind, flags)
+    make_options = {
+        "SLP_PARAMETER_CFLAGS": flags,
+        "USE_SERIAL_PRINTF": 1
+    }
+
+    # If this is a build for a testbed, make sure to pass that information
+    # on to the makefile, which may need to do additional things to support that testbed.
+    if "TESTBED" in kwargs:
+        make_options["TESTBED"] = kwargs["TESTBED"]
+
+    make_options_string = " ".join('{}={}'.format(k, repr(v)) for (k, v) in make_options.items())
+
+    command = 'make {} {}'.format(platform, make_options_string)
+
+    print(command)
 
     result = subprocess.check_call(
         command,
