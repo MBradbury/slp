b92d73bc79d99f0d99f233c7cbeb9949bba86a1c
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-08-25 16:40:12 +0100

    Switch to using the first 2500 lines of the noise models

--- simulator/Simulation.py
+++ simulator/Simulation.py
@@ -234,7 +234,7 @@ class Simulation(object):
 
         # Instead of reading in all the noise data, a limited amount
         # is used. If we were to use it all it leads to large slowdowns.
-        count = 10000
+        count = 2500
 
         noises = list(islice(self._read_noise_from_file(path), count))
 
