35de99fcc53988e0d82869ac987ee729a1eb0232
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-08-31 11:21:37 +0100

    Mark NodeWasSource as allow_missing, as we currently do not attempt to average it.

--- algorithm/probrate/Analysis.py
+++ algorithm/probrate/Analysis.py
@@ -24,7 +24,7 @@ class Analyzer(AnalyzerCommon):
         d['dummy normal']       = lambda x: AnalyzerCommon._format_results(x, 'DummyNormalSent')
         d['ssd']                = lambda x: AnalyzerCommon._format_results(x, 'NormalSinkSourceHops')
 
-        d['node was source']    = lambda x: AnalyzerCommon._format_results(x, 'NodeWasSource')
+        d['node was source']    = lambda x: AnalyzerCommon._format_results(x, 'NodeWasSource', allow_missing=True)
         
         d['sent heatmap']       = lambda x: AnalyzerCommon._format_results(x, 'SentHeatMap')
         d['received heatmap']   = lambda x: AnalyzerCommon._format_results(x, 'ReceivedHeatMap')
