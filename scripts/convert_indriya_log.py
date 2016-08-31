244baf4af30b99ed232ab69b6ccbdd96e1091079
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-08-31 21:00:43 +0100

    Work on new indriya log format converter

--- scripts/convert_indriya_log.py
+++ scripts/convert_indriya_log.py
@@ -1,31 +1,68 @@
 #!/usr/bin/env python
+from __future__ import print_function, division
 
-from __future__ import print_function
+import glob
+import os
 
-import csv
-import sys
+import pandas
+
+# The node types. See algorithm/common/SerialMetricLoggingTypes.h for the definition
+AM_METRIC_RECEIVE_MSG = 50
+AM_METRIC_BCAST_MSG = 51
+AM_METRIC_DELIVER_MSG = 52
+AM_ATTACKER_RECEIVE_MSG = 53
+AM_METRIC_NODE_CHANGE_MSG = 54
+AM_ERROR_OCCURRED_MSG = 54
+
+message_types_to_channels = {
+    AM_METRIC_RECEIVE_MSG: "M-C",
+    AM_METRIC_BCAST_MSG: "M-C",
+    AM_METRIC_DELIVER_MSG: "M-C",
+    AM_ATTACKER_RECEIVE_MSG: "A-R",
+    AM_METRIC_NODE_CHANGE_MSG: "M-NC",
+    AM_ERROR_OCCURRED_MSG: "stderr",
+}
+
+def _read_dat_file(path):
+    try:
+        reader = pandas.read_csv(path, delimiter="\t", parse_dates=True)
+
+        # Check there is only one message type
+        if len(reader["type"].unique()) != 1:
+            raise RuntimeError("The type column has more than 1 value.")
+
+        message_type = reader["type"][0]
+
+        # Don't need the type column any more
+        del reader["type"]
+
+        #reader["milli_time"] -= reader["milli_time"][0]
+
+        return (message_type, reader)
+    except pandas.io.common.EmptyDataError:
+        # Skip empty files
+        return (None, None)
+
+def convert_indriya_log(directory, output_file):
+
+    dat_file_paths = glob.glob(os.path.join(directory, "*.dat"))
+
+    dat_files = {message_type: reader
+                 for (message_type, reader)
+                 in (_read_dat_file(path) for path in dat_file_paths)
+                 if message_type is not None
+    }
+
+    print(dat_files)
 
 if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(description="Indriya Result Converter", add_help=True)
+    parser.add_argument("--result-dir", type=str, required=True, help="The location of the main results files.")
+    parser.add_argument("--output-file", type=str, required=True, help="The location of the merged result file.")
+
+    args = parser.parse_args(sys.argv[1:])
     
-    filename = sys.argv[1]
-
-    with open(filename, 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-
-        for line in reader:
-            (data_line, date, nid, time_ms, no) = line
-
-            # TODO: use time_ms to create the time
-
-            # Skip first line
-            if no == "motelabSeqNo":
-                continue
-
-            data_line = data_line.replace("\\0", "")
-            nid = int(nid)
-
-            # The first line needs a date and time too
-            if no == "1":
-                data_line = date + ":" + data_line
-
-            print(data_line.replace("\\n", "\n{}:".format(date)), end='')
+    convert_indriya_log(args.result_dir, args.output_file)
