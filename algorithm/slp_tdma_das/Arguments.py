c531d1e39189b79598c432e7ae06107553ad6f4b
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-09-05 11:20:19 +0100

    Fix the name of --pre-beacon-periods

--- algorithm/slp_tdma_das/Arguments.py
+++ algorithm/slp_tdma_das/Arguments.py
@@ -10,11 +10,11 @@ class Arguments(ArgumentsCommon):
         self.add_argument("--source-period",
                           type=simulator.SourcePeriodModel.eval_input, required=True)
         self.add_argument("-sp", "--slot-period", type=float, required=True, help="Time of a single slot")
-        self.add_argument("-dp", "--dissem-period", type=float, required=True, help="Time of the beaconing period")
+        self.add_argument("-dp", "--dissem-period", type=float, required=True, help="Time of the beacon period")
         self.add_argument("-ts", "--tdma-num-slots", type=int, required=True, help="Total number of slots available")
         self.add_argument("-ai", "--slot-assignment-interval", type=int, required=True, help="The interval at which slot values are assigned")
         self.add_argument("-msp", "--minimum-setup-periods", type=int, required=False, default=0, help="Minimum number of periods required for setup")
-        self.add_argument("-pbp", "--pre-beaconing-periods", type=int, required=False, default=3, help="Number of periods of neighbour discovery")
+        self.add_argument("-pbp", "--pre-beacon-periods", type=int, required=False, default=3, help="Number of periods of neighbour discovery")
         self.add_argument("-dt", "--dissem-timeout", type=int, required=False, default=5, help="Timeout to stop sending dissem messages")
         self.add_argument("--source-mobility",
                           type=simulator.MobilityModel.eval_input,
@@ -28,7 +28,7 @@ class Arguments(ArgumentsCommon):
         result["TDMA_NUM_SLOTS"] = self.args.tdma_num_slots
         result["SLOT_ASSIGNMENT_INTERVAL"] = self.args.slot_assignment_interval
         result["TDMA_SETUP_PERIODS"] = self.args.minimum_setup_periods
-        result["TDMA_PRE_BEACON_PERIODS"] = self.args.pre_beaconing_periods
+        result["TDMA_PRE_BEACON_PERIODS"] = self.args.pre_beacon_periods
         result["TDMA_DISSEM_TIMEOUT"] = self.args.dissem_timeout
 
         return result
