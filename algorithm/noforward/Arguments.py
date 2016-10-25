68e3ed606a5e678456805f85f5f691b81fc6598b
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-10-25 13:14:27 +0100

    NoForward: Style fixes

--- algorithm/noforward/Arguments.py
+++ algorithm/noforward/Arguments.py
@@ -8,7 +8,7 @@ class Arguments(ArgumentsCommon):
         super(Arguments, self).__init__("SLP No Forward", has_safety_period=True)
 
         self.add_argument("--source-period",
-            type=simulator.SourcePeriodModel.eval_input, required=True)
+                          type=simulator.SourcePeriodModel.eval_input, required=True)
         self.add_argument("--source-mobility",
-            type=simulator.MobilityModel.eval_input,
-            default=simulator.MobilityModel.StationaryMobilityModel())
+                          type=simulator.MobilityModel.eval_input,
+                          default=simulator.MobilityModel.StationaryMobilityModel())
