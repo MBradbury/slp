12a28d8072b9b943f45f9b9284e6f578ad444224
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-10-25 13:14:45 +0100

    Refactor table formatters into their own class

---
+++ data/table/data_formatter.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+
+import sys
+
+from data import latex
+
+class TableDataFormatter(object):
+    def __init__(self):
+        pass
+
+    def format_header(self, name):
+        try:
+            return {
+                "network size": ("Network Size", "nodes"),
+                "repeats": ("Repeats", "~"),
+
+                "source period": ("$P_{src}$", "(sec)"),
+                "fake period": ("$P_{fs}$", "(sec)"),
+                "temp fake duration": ("Dur", "(sec)"),
+                "pr(tfs)": ("P[TFS]", "(\\%)"),
+                "pr(pfs)": ("P[PFS]", "(\\%)"),
+                "short walk length": ("Short Walk Length", "(hops)"),
+                "long walk length": ("Long Walk Length", "(hops)"),
+                "captured": ("Cap", "(\\%)"),
+                "fake": ("Fake", "Messages"),
+                "dummy normal": ("Dummy Normal", "Messages"),
+                "received ratio": ("Received", "(\\%)"),
+                "tfs": ("TFS", "~"),
+                "pfs": ("PFS", "~"),
+                "tailfs": ("TailFS", "~"),
+                "pull back hops": ("Pull Back", "Messages"),
+                "ssd": ("$\\Delta_{ss}$", "(hops)"),
+                "normal latency": ("Laten", "(msec)"),
+                "approach": ("Approach", "~"),
+                "time taken": ("Time", "(sec)"),
+                "safety period": ("Safety Period", "(sec)"),
+                "wall time": ("Wall Time", "(sec)"),
+                "total wall time": ("Total Wall Time", "(sec)"),
+                "event count": ("Event Count", ""),
+                
+                "walk length": ("Walk Length", "(hops)"),
+                "walk retries": ("Walk", "Retries"),
+                "paths reached end": ("Paths Ended", "(\\%)"),
+
+                "norm(sent,time taken)": ("$M$ $T^{-1}$", "~"),
+                "norm(norm(sent,time taken),num_nodes)": ("$M$ $T^{-1}$ $\\Sigma^{-1}$", "~"),
+                "norm(norm(norm(sent,time taken),num_nodes),source_rate)": ("$M$ $T^{-1}$ $\\Sigma^{-1}$ $R^{-1}$", "~"),
+            }[name]
+        except KeyError as ex:
+            print("Failed to find the name '{}' for row {}. Using default. : {}".format(name, row, ex), file=sys.stderr)
+            return name
+
+    def format_value(self, name, value):
+        if value is None:
+            return "None"
+        elif name in {"source period", "fake period", "walk length", "walk retries",
+                      "repeats", "short walk length", "long walk length"}:
+            return "${}$".format(value)
+        elif name in {"duration", "temp fake duration", "captured"}:
+            return "${:.0f}$".format(value)
+        elif name == "pr(tfs)" or name == "pr(pfs)":
+            return "${:.0f}$".format(value * 100.0)
+        elif name in {"tfs", "pfs", "tailfs"}:
+            return "${:.1f}$".format(value[0])
+        elif name == "approach":
+            return latex.escape(value.replace("_APPROACH", ""))
+        elif name in {"landmark node"}:
+            return latex.escape(value)
+        elif name in {"received ratio", "ssd", "paths reached end"}:
+            return "${:.1f} \\pm {:.1f}$".format(value[0], value[1])
+        elif name in {"sent", "received", "delivered",
+                      "fake", "away", "choose", "dummy normal",
+                      "normal latency"}:
+            return "${:.0f} \\pm {:.0f}$".format(value[0], value[1])
+        elif isinstance(value, dict):
+            return latex.escape(str(value))
+        elif isinstance(value, float):
+            return "${:.2f}$".format(value)
+        else:
+            try:
+                return "${:.3f} \\pm {:.3f}$".format(value[0], value[1])
+            except TypeError as e:
+                raise RuntimeError("Unable to format values for {} with values {} under the default setting".format(name, value), e)
