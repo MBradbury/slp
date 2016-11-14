2cd7b6f29ab82aabf9de8b98003711604ce49076
Matthew Bradbury <MBradbury@users.noreply.github.com>
2016-11-14 16:03:56 +0000

    Format capture ratio with 2dp

--- data/table/data_formatter.py
+++ data/table/data_formatter.py
@@ -63,7 +63,7 @@ class TableDataFormatter(object):
         elif name in {"source period", "fake period", "walk length", "walk retries",
                       "repeats", "short walk length", "long walk length"}:
             return "${}$".format(value)
-        elif name in {"duration", "temp fake duration", "captured"}:
+        elif name in {"duration", "temp fake duration"}:
             return "${:.0f}$".format(value)
         elif name == "pr(tfs)" or name == "pr(pfs)":
             return "${:.0f}$".format(value * 100.0)
