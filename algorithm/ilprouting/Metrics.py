7d2eb509b4701d928be802fd4fee209b1f3ade62
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-11-17 16:18:42 +0000

    ILPRouting: Initial work on algorithm

---
+++ algorithm/ilprouting/Metrics.py
@@ -0,0 +1,12 @@
+from __future__ import print_function, division
+
+from simulator.MetricsCommon import MetricsCommon
+
+class Metrics(MetricsCommon):
+    def __init__(self, sim, configuration):
+        super(Metrics, self).__init__(sim, configuration)
+
+    @staticmethod
+    def items():
+        d = MetricsCommon.items()
+        return d
