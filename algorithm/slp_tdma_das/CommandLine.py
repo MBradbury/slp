db44e359061bb162b002a5752e016858845feb08
Jack Kirton <3943894+jack-kirton@users.noreply.github.com>
2016-12-05 20:50:58 +0000

    SLP TDMA DAS: Update command line parameters and bug fixes

--- algorithm/slp_tdma_das/CommandLine.py
+++ algorithm/slp_tdma_das/CommandLine.py
@@ -1,8 +1,10 @@
-from __future__ import print_function
+from __future__ import print_function, division
 
 import itertools
 
 from simulator import CommandLineCommon
+
+import algorithm
 
 protectionless_tdma_das = algorithm.import_algorithm("protectionless_tdma_das")
 
@@ -11,14 +13,19 @@ from data.table import safety_period
 
 class RunSimulations(RunSimulationsCommon):
     def _get_safety_period(self, darguments):
-        time_taken = super(RunSimulations, self)._get_safety_period(self, darguments)
+        time_taken = super(RunSimulations, self)._get_safety_period(darguments)
 
-        minimum_setup_period = darguments["minimum setup periods"]
-        tdma_safety_period = darguments["tdma safety periods"]
+        # minimum_setup_period = darguments["minimum setup periods"]
+        # tdma_safety_period = darguments["tdma safety periods"]
+        # dissem_period = darguments["dissem period"]
+        # slot_period = darguments["slot period"]
+        # tdma_num_slots = darguments["tdma num slots"]
 
-        period_length_sec = 3
+        # period_length_sec = dissem_period + (slot_period * tdma_num_slots)
+        # tdma_safety_period = int(time_taken / period_length_sec) - minimum_setup_periods + 1
 
-        return (minimum_setup_period + tdma_safety_period) * period_length_sec
+        # return (minimum_setup_period + tdma_safety_period) * period_length_sec
+        return time_taken
 
 class CLI(CommandLineCommon.CLI):
     def __init__(self):
@@ -31,16 +38,25 @@ class CLI(CommandLineCommon.CLI):
             parameters.sizes, parameters.configurations,
             parameters.attacker_models, parameters.noise_models, parameters.communication_models,
             [parameters.distance], parameters.node_id_orders, [parameters.latest_node_start_time],
-            parameters.source_periods, parameters.slot_periods, parameters.dissem_periods,
-            parameters.tdma_num_slots, parameters.slot_assignment_intervals, parameters.minimum_setup_periods,
-            parameters.pre_beacon_periods, parameters.dissem_timeouts, parameters.tdma_safety_periods_and_search_distances
+            parameters.source_periods, parameters.slot_period, parameters.dissem_period,
+            parameters.tdma_num_slots, parameters.slot_assignment_interval, parameters.minimum_setup_periods,
+            parameters.pre_beacon_periods, parameters.dissem_timeout, parameters.search_distance
         ))
 
-        argument_product = [
-                (s, c, am, nm, cm, d, nido, lnst, src_period, sp, dp, ts, ai, msp, pbp, dt, sd, tsp)
-            for (s, c, am, nm, cm, d, nido, lnst, src_period, sp, dp, ts, ai, msp, pbp, dt, (tsp, sd))
-            in  argument_product
-        ]
+        # argument_product = list(itertools.product(
+            # parameters.sizes, parameters.configurations,
+            # parameters.attacker_models, parameters.noise_models, parameters.communication_models,
+            # [parameters.distance], parameters.node_id_orders, [parameters.latest_node_start_time],
+            # parameters.source_periods, parameters.slot_periods, parameters.dissem_periods,
+            # parameters.tdma_num_slots, parameters.slot_assignment_intervals, parameters.minimum_setup_periods,
+            # parameters.pre_beacon_periods, parameters.dissem_timeouts, parameters.tdma_safety_periods_and_search_distances
+        # ))
+
+        # argument_product = [
+                # (s, c, am, nm, cm, d, nido, lnst, src_period, sp, dp, ts, ai, msp, pbp, dt, sd, tsp)
+            # for (s, c, am, nm, cm, d, nido, lnst, src_period, sp, dp, ts, ai, msp, pbp, dt, (tsp, sd))
+            # in  argument_product
+        # ]
 
         argument_product = self.adjust_source_period_for_multi_source(argument_product)
 
