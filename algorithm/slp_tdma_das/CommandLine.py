8b8c8408b9a4d81071beabd0e8092a8704344c4c
Jack Kirton <3943894+jack-kirton@users.noreply.github.com>
2016-12-12 14:44:17 +0000

    SLP TDMA DAS: Add time estimator for cluster jobs

--- algorithm/slp_tdma_das/CommandLine.py
+++ algorithm/slp_tdma_das/CommandLine.py
@@ -39,6 +39,23 @@ class CLI(CommandLineCommon.CLI):
         subparser = self._subparsers.add_parser("graph")
         subparser = self._subparsers.add_parser("graph-versus-baseline")
 
+    def _time_estimater(self, args, **kwargs):
+        """Estimates how long simulations are run for. Override this in algorithm
+        specific CommandLine if these values are too small or too big. In general
+        these have been good amounts of time to run simulations for. You might want
+        to adjust the number of repeats to get the simulation time in this range."""
+        size = args['network size']
+        if size == 11:
+            return datetime.timedelta(hours=1)
+        elif size == 15:
+            return datetime.timedelta(hours=1)
+        elif size == 21:
+            return datetime.timedelta(hours=1)
+        elif size == 25:
+            return datetime.timedelta(hours=1)
+        else:
+            raise RuntimeError("No time estimate for network sizes other than 11, 15, 21 or 25")
+
     def _argument_product(self):
         parameters = self.algorithm_module.Parameters
 
