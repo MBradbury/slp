54a4cb3ea88099f17c9fc4653e6165f6178c714b
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-01-17 12:17:21 +0000

    ILP Routing: 1) Add a maximum walk distance. 2) Add probabilistic source sending direct to sink. 3) Change how the sink pull back works.

--- algorithm/ilprouting/Arguments.py
+++ algorithm/ilprouting/Arguments.py
@@ -20,8 +20,11 @@ class Arguments(ArgumentsCommon):
                           default=simulator.MobilityModel.StationaryMobilityModel())
 
         self.add_argument("--max-buffer-size", type=buffer_size, required=True)
+        self.add_argument("--max-walk-length", type=self.type_positive_int, required=True)
 
-        self.add_argument("--target-latency", type=float, required=True)
+        self.add_argument("--target-latency", type=self.type_positive_float, required=True)
+
+        self.add_argument("--pr-direct-to-sink", type=self.type_probability, required=True)
 
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
@@ -30,5 +33,9 @@ class Arguments(ArgumentsCommon):
         result["SLP_TARGET_LATENCY_MS"] = int(self.args.target_latency * 1000)
 
         result["SLP_FASTEST_SOURCE_PERIOD_MS"] = int(self.args.source_period.fastest() * 1000)
+
+        result["SLP_MAX_WALK_LENGTH"] = self.args.max_walk_length
+
+        result["SLP_PR_SEND_DIRECT_TO_SINK"] = self.args.pr_direct_to_sink
 
         return result
