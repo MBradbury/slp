c7cf08b633b4f438054538ddbf6fbe84b64f0303
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-01-18 14:29:26 +0000

    ILP Routing: Improve delivery ratio by avoiding resending to nodes that have previously failed a number of times.

--- algorithm/ilprouting/MessageQueueInfo.h
+++ algorithm/ilprouting/MessageQueueInfo.h
@@ -1,6 +1,8 @@
 #ifndef SLP_MESSAGE_QUEUE_INFO_H
 #define SLP_MESSAGE_QUEUE_INFO_H
 
+#include "Constants.h"
+
 typedef struct message_queue_info
 {
 	message_t msg;
@@ -10,6 +12,14 @@ typedef struct message_queue_info
 	uint8_t calculate_target_attempts;
 	bool ack_requested;
 
+	// A list of neighbours we have failed to send to
+	am_addr_t failed_neighbour_sends[RTX_ATTEMPTS];
+
 } message_queue_info_t;
+
+inline uint8_t failed_neighbour_sends_length(const message_queue_info_t* info)
+{
+	return RTX_ATTEMPTS - info->rtx_attempts;
+}
 
 #endif // SLP_MESSAGE_QUEUE_INFO_H
