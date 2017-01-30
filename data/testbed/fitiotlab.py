e60e1bc657ad2ab4e552f53e7f65b65284ea7665
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-01-30 14:55:20 +0000

    Add a number of placeholder topologies to fitiotlab that contain nodes that we can deploy to

--- data/testbed/fitiotlab.py
+++ data/testbed/fitiotlab.py
@@ -31,3 +31,82 @@ def url():
 #
 # After you have done #2, you could just do the following locally:
 # $ ssh <login>@<site>.iot-lab.info "serial_aggregator -i <experiment_id>"
+
+# Strasbourg - 3D grid of nodes - https://www.iot-lab.info/deployment/strasbourg/
+# Rennes - Unknown - https://www.iot-lab.info/deployment/rennes/
+# Saclay - Unknown - https://www.iot-lab.info/deployment/saclay/
+
+class Grenoble(Topology):
+    """The layout of nodes on the Grenbole testbed, see: https://www.iot-lab.info/testbed/maps.php?site=grenoble"""
+    def __init__(self):
+        super(Grenoble, self).__init__()
+
+        floor_distance = 20.0
+
+        for nid in xrange(1, 139):
+            self.nodes[nid] = np.array((-100, -100), dtype=np.float64)
+
+        self._process_node_id_order("topology")
+
+    def __str__(self):
+        return "Grenoble<>"
+
+class Rennes(Topology):
+    """The layout of nodes on the Rennes testbed, see: https://www.iot-lab.info/testbed/maps.php?site=rennes"""
+    def __init__(self):
+        super(Rennes, self).__init__()
+
+        floor_distance = 20.0
+
+        for nid in xrange(1, 139):
+            self.nodes[nid] = np.array((-100, -100), dtype=np.float64)
+
+        self._process_node_id_order("topology")
+
+    def __str__(self):
+        return "Rennes<>"
+
+class Euratech(Topology):
+    """The layout of nodes on the Euratech testbed, see: https://www.iot-lab.info/testbed/maps.php?site=euratech"""
+    def __init__(self):
+        super(Euratech, self).__init__()
+
+        floor_distance = 20.0
+
+        for nid in xrange(1, 139):
+            self.nodes[nid] = np.array((-100, -100), dtype=np.float64)
+
+        self._process_node_id_order("topology")
+
+    def __str__(self):
+        return "Euratech<>"
+
+class Strasbourg(Topology):
+    """The layout of nodes on the Strasbourg testbed, see: https://www.iot-lab.info/testbed/maps.php?site=strasbourg"""
+    def __init__(self):
+        super(Strasbourg, self).__init__()
+
+        floor_distance = 20.0
+
+        for nid in xrange(1, 139):
+            self.nodes[nid] = np.array((-100, -100), dtype=np.float64)
+
+        self._process_node_id_order("topology")
+
+    def __str__(self):
+        return "Strasbourg<>"
+
+class Saclay(Topology):
+    """The layout of nodes on the Saclay testbed, see: https://www.iot-lab.info/testbed/maps.php?site=saclay"""
+    def __init__(self):
+        super(Saclay, self).__init__()
+
+        floor_distance = 20.0
+
+        for nid in xrange(1, 139):
+            self.nodes[nid] = np.array((-100, -100), dtype=np.float64)
+
+        self._process_node_id_order("topology")
+
+    def __str__(self):
+        return "Saclay<>"
