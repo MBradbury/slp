6a4d957145567c44a5346aae745485f0117e3c20
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-02-03 12:04:29 +0000

    ILP Routing: Add useful normalised results to analysis

--- algorithm/ilprouting/Analysis.py
+++ algorithm/ilprouting/Analysis.py
@@ -9,7 +9,14 @@ class Analyzer(AnalyzerCommon):
 
     @staticmethod
     def normalised_parameters():
-        return tuple()
+        return (
+            ('Sent', 'TimeTaken'),
+            ('NormalSent', 'TimeTaken'),
+
+            ('energy_impact', 'num_nodes'),
+            (('energy_impact', 'num_nodes'), 'TimeTaken'),
+            ('daily_allowance_used', '1'),
+        )
 
     @staticmethod
     def results_header():
@@ -26,5 +33,12 @@ class Analyzer(AnalyzerCommon):
 
         d['sent heatmap']       = lambda x: AnalyzerCommon._format_results(x, 'SentHeatMap')
         d['received heatmap']   = lambda x: AnalyzerCommon._format_results(x, 'ReceivedHeatMap')
+
+        d['norm(sent,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(Sent,TimeTaken)')
+        d['norm(normal,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(NormalSent,TimeTaken)')
+
+        d['energy impact per node']   = lambda x: AnalyzerCommon._format_results(x, 'norm(energy_impact,num_nodes)')
+        d['energy impact per node per second']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(energy_impact,num_nodes),TimeTaken)')
+        d['energy allowance used'] = lambda x: AnalyzerCommon._format_results(x, 'norm(daily_allowance_used,1)')
 
         return d
