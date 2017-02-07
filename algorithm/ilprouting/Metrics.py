9c69e62e8403a3aa483064e9c88220fa5bde0e1c
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-02-07 12:32:07 +0000

    ILP Routing: Add another metric, the number of rtx

--- algorithm/ilprouting/Metrics.py
+++ algorithm/ilprouting/Metrics.py
@@ -2,6 +2,9 @@ from __future__ import print_function, division
 
 from simulator.MetricsCommon import MetricsCommon
 
+ERROR_RTX_FAILED = 12
+ERROR_RTX_FAILED_TRYING_OTHER = 13
+
 class Metrics(MetricsCommon):
     def __init__(self, sim, configuration):
         super(Metrics, self).__init__(sim, configuration)
@@ -14,7 +17,7 @@ class Metrics(MetricsCommon):
         d["BeaconSent"]             = lambda x: x.number_sent("Beacon")
         d["PollSent"]               = lambda x: x.number_sent("Poll")
 
-        # 13 is ERROR_RTX_FAILED_TRYING_OTHER
-        d["FailedAvoidSink"]        = lambda x: x.errors[13] / x.num_normal_sent_if_finished()
+        d["FailedRtx"]              = lambda x: x.errors[ERROR_RTX_FAILED] + x.errors[ERROR_RTX_FAILED_TRYING_OTHER]
+        d["FailedAvoidSink"]        = lambda x: x.errors[ERROR_RTX_FAILED_TRYING_OTHER] / x.num_normal_sent_if_finished()
 
         return d
