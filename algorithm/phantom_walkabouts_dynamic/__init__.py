5659456f542ab25d6edc3b8cbcc39547c1669688
Chen Gu <c.gu.warwick@warwick.ac.uk>
2017-02-11 18:19:37 +0000

    phantom_walkabouts_dynamic: remove local parameters

--- algorithm/phantom_walkabouts_dynamic/__init__.py
+++ algorithm/phantom_walkabouts_dynamic/__init__.py
@@ -6,6 +6,4 @@ def _setup():
 
 (name, results_path, result_file, result_file_path, graphs_path) = _setup()
 
-
-local_parameter_names = ('direction bias',
-                         'order', 'wait before short', 'dynamic period repeat')
+local_parameter_names = ()
