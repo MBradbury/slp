b889d790114dcb40b43a4dce52267b56779d3177
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-02-20 13:40:16 +0000

    Template: Add more normalised results to generate

--- algorithm/template/Analysis.py
+++ algorithm/template/Analysis.py
@@ -9,7 +9,18 @@ class Analyzer(AnalyzerCommon):
 
     @staticmethod
     def normalised_parameters():
-        return tuple()
+        return (
+            ('Sent', 'TimeTaken'),
+            (('Sent', 'TimeTaken'), 'num_nodes'),
+            ((('Sent', 'TimeTaken'), 'num_nodes'), 'source_rate'),
+            ('FakeSent', 'TimeTaken'),
+            (('FakeSent', 'TimeTaken'), 'source_rate'),
+            ('NormalSent', 'TimeTaken'),
+
+            ('energy_impact', 'num_nodes'),
+            (('energy_impact', 'num_nodes'), 'TimeTaken'),
+            ('daily_allowance_used', '1'),
+        )
 
     @staticmethod
     def results_header():
@@ -27,5 +38,18 @@ class Analyzer(AnalyzerCommon):
 
         d['sent heatmap']       = lambda x: AnalyzerCommon._format_results(x, 'SentHeatMap')
         d['received heatmap']   = lambda x: AnalyzerCommon._format_results(x, 'ReceivedHeatMap')
+
+        d['norm(sent,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(Sent,TimeTaken)')
+        d['norm(norm(sent,time taken),network size)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(Sent,TimeTaken),num_nodes)')
+        d['norm(norm(norm(sent,time taken),network size),source rate)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(norm(Sent,TimeTaken),num_nodes),source_rate)')
+
+        d['norm(fake,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(FakeSent,TimeTaken)')
+        d['norm(norm(fake,time taken),source rate)'] = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(FakeSent,TimeTaken),source_rate)')
+
+        d['norm(normal,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(NormalSent,TimeTaken)')
+
+        d['energy impact per node']   = lambda x: AnalyzerCommon._format_results(x, 'norm(energy_impact,num_nodes)')
+        d['energy impact per node per second']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(energy_impact,num_nodes),TimeTaken)')
+        d['energy allowance used'] = lambda x: AnalyzerCommon._format_results(x, 'norm(daily_allowance_used,1)')
 
         return d
