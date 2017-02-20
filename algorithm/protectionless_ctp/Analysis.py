aba4fa8a774f8d1c3f92e08a9ce75417ede86e70
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-02-20 13:40:55 +0000

    Protectionless CTP: Add more normalised results to analyse

--- algorithm/protectionless_ctp/Analysis.py
+++ algorithm/protectionless_ctp/Analysis.py
@@ -1,37 +1,7 @@
 
-from data.analysis import Analyse, AnalysisResults, AnalyzerCommon
-
-from simulator import SourcePeriodModel
+from data.analysis import AnalyzerCommon
 
 algorithm_module = __import__(__package__, globals(), locals(), ['object'], -1)
-
-class AnalyseWithOutlierDetection(Analyse):
-    def detect_outlier(self, values):
-        return None
-
-        # Disable outlier detection, as we handle failing to capture
-        # the source differently now
-        """# Discard simulations that didn't capture the source
-        captured_index = self.headings.index("Captured")
-        captured = bool(values[captured_index])
-
-        if not captured:
-            raise RuntimeError("Detected outlier, the source was not captured")
-
-        # Discard simulations that took too long
-        time_index = self.headings.index("TimeTaken")
-        time_taken = float(values[time_index])
-
-        network_size = int(self.opts['network_size'])
-        source_period = float(SourcePeriodModel.eval_input(self.opts['source_period']))
-
-        upper_bound = (network_size ** 2) * source_period
-
-        # This can be much stricter than the protectionless upper bound on time.
-        # As it can be changed once the simulations have been run.
-        if time_taken >= upper_bound:
-            raise RuntimeError("Detected outlier, the time taken is {}, upper bound is {}".format(
-                time_taken, upper_bound))"""
 
 class Analyzer(AnalyzerCommon):
     def __init__(self, results_directory):
@@ -41,8 +11,13 @@ class Analyzer(AnalyzerCommon):
     def normalised_parameters():
         return (
             ('Sent', 'TimeTaken'),
+            (('Sent', 'TimeTaken'), 'num_nodes'),
+
             ('NormalSent', 'TimeTaken'),
-            ('TimeTaken', 'source_period')
+
+            ('energy_impact', 'num_nodes'),
+            (('energy_impact', 'num_nodes'), 'TimeTaken'),
+            ('daily_allowance_used', '1'),
         )
 
     @staticmethod
@@ -59,10 +34,12 @@ class Analyzer(AnalyzerCommon):
         d['received heatmap']   = lambda x: AnalyzerCommon._format_results(x, 'ReceivedHeatMap')
 
         d['norm(sent,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(Sent,TimeTaken)')
+        d['norm(norm(sent,time taken),num_nodes)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(Sent,TimeTaken),num_nodes)')
+       
         d['norm(normal,time taken)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(NormalSent,TimeTaken)')
-        d['norm(time taken,source period)']   = lambda x: AnalyzerCommon._format_results(x, 'norm(TimeTaken,source_period)')
+
+        d['energy impact per node']   = lambda x: AnalyzerCommon._format_results(x, 'norm(energy_impact,num_nodes)')
+        d['energy impact per node per second']   = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(energy_impact,num_nodes),TimeTaken)')
+        d['energy allowance used'] = lambda x: AnalyzerCommon._format_results(x, 'norm(daily_allowance_used,1)')
 
         return d
-
-    #def analyse_path(self, path):
-    #    return AnalyseWithOutlierDetection(path)
