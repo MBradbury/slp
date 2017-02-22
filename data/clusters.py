1c0a4558219d575d89280a4d88bb2e49a884548b
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-02-22 16:25:00 +0000

    Increase memory reserved for cluster os

--- data/clusters.py
+++ data/clusters.py
@@ -66,7 +66,7 @@ class ClusterCommon(object):
         return raw_input("Enter your {} username: ".format(self.name().title()))
 
 
-    def _ram_to_ask_for(self, ram_for_os_mb=2 * 1024):
+    def _ram_to_ask_for(self, ram_for_os_mb=4 * 1024):
         return int(math.floor(((self.ram_per_node * self.ppn) - ram_for_os_mb) / self.ppn)) * self.ppn
 
     def _pbs_submitter(self, notify_emails=None):
