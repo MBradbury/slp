21617dbf981b9ba8bc0c157b26873f18ace266e4
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-03-01 16:23:36 +0000

    When running jobs also call lscpu before running. This should help obtain more information on the cluster's hardware

--- data/run/driver/cluster_submitter.py
+++ data/run/driver/cluster_submitter.py
@@ -47,7 +47,8 @@ class Runner(object):
             'echo "TOSROOT: $TOSROOT"',
             'echo "PYTHONPATH: $PYTHONPATH"',
             'python --version',
-            'python -c "import scipy, numpy; print(scipy.__name__, scipy.__version__); print(numpy.__name__, numpy.__version__)"'
+            'python -c "import scipy, numpy; print(scipy.__name__, scipy.__version__); print(numpy.__name__, numpy.__version__)"',
+            'lscpu'
         ]
         debug_command = " ; ".join(debug_commands)
         
