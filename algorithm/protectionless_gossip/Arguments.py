5334a936269423e7cbc27c48e407f37b1b60116c
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-04-10 09:44:47 +0100

    Protectionless Gossip: AdaptiveSPR Gossip: AdaptiveSPR CTP Gossip: Change gossip period to be in seconds

--- algorithm/protectionless_gossip/Arguments.py
+++ algorithm/protectionless_gossip/Arguments.py
@@ -5,7 +5,7 @@ import simulator.MobilityModel
 
 class Arguments(ArgumentsCommon):
     def __init__(self):
-        super(Arguments, self).__init__("SLP Protectionless", has_safety_period=False)
+        super(Arguments, self).__init__("SLP Protectionless Gossip", has_safety_period=False)
 
         self.add_argument("--source-period",
             type=simulator.SourcePeriodModel.eval_input, required=True)
@@ -13,11 +13,11 @@ class Arguments(ArgumentsCommon):
             type=simulator.MobilityModel.eval_input,
             default=simulator.MobilityModel.StationaryMobilityModel())
 
-        self.add_argument("--gossip-period", type=int, required=True)
+        self.add_argument("--gossip-period", type=self.type_positive_float, required=True)
 
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
 
-        result["GOSSIP_PERIOD_MS"] = self.args.gossip_period
+        result["GOSSIP_PERIOD_MS"] = int(self.args.gossip_period * 1000)
 
         return result
