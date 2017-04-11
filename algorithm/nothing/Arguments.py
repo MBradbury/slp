553dc557401ab66cca94293b0bd49ebc21d8c44f
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2017-04-11 13:34:48 +0100

    Nothing doesn't have a safety period

--- algorithm/nothing/Arguments.py
+++ algorithm/nothing/Arguments.py
@@ -5,7 +5,7 @@ import simulator.MobilityModel
 
 class Arguments(ArgumentsCommon):
     def __init__(self):
-        super(Arguments, self).__init__("SLP No Forward", has_safety_period=True)
+        super(Arguments, self).__init__("SLP No Forward", has_safety_period=False)
 
         self.add_argument("--source-period",
                           type=simulator.SourcePeriodModel.eval_input, required=True)
