556bc91540556650f0d3c46d4cc5fc6c5649aea8
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-04-12 10:32:34 +0100

    Support default vary titles when vary is a tuple

--- simulator/CommandLineCommon.py
+++ simulator/CommandLineCommon.py
@@ -276,7 +276,7 @@ class CLI(object):
 
                 g.xaxis_label = xaxis.title()
                 g.yaxis_label = yaxis_label
-                g.vary_label = vary.title()
+                g.vary_label = "/".join(x.title() for x in vary) if isinstance(vary, tuple) else vary.title()
                 g.vary_prefix = vary_units
                 g.key_position = key_position
 
@@ -337,7 +337,7 @@ class CLI(object):
 
                 g.xaxis_label = xaxis.title()
                 g.yaxis_label = yaxis_label
-                g.vary_label = vary.title()
+                g.vary_label = "/".join(x.title() for x in vary) if isinstance(vary, tuple) else vary.title()
                 g.vary_prefix = vary_units
                 g.key_position = key_position
 
