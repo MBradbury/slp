15742f2fe1b55ddd0b88da512aece60b7a7fb7e3
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-04-13 11:56:08 +0100

    AdaptiveSPR Notify: Reimplement based on changed AdaptiveSPR with flooding for notify

--- algorithm/adaptive_spr_notify/Arguments.py
+++ algorithm/adaptive_spr_notify/Arguments.py
@@ -6,21 +6,19 @@ approaches = ["PB_FIXED1_APPROACH", "PB_FIXED2_APPROACH", "PB_RND_APPROACH"]
 
 class Arguments(ArgumentsCommon):
     def __init__(self):
-        super(Arguments, self).__init__("SLP Adaptive SPR", has_safety_period=True)
+        super(Arguments, self).__init__("SLP Adaptive SPR Notify", has_safety_period=True)
 
         self.add_argument("--source-period", type=float, required=True)
         self.add_argument("--source-mobility",
-                            type=simulator.MobilityModel.eval_input,
-                            default=simulator.MobilityModel.StationaryMobilityModel())
+                          type=simulator.MobilityModel.eval_input,
+                          default=simulator.MobilityModel.StationaryMobilityModel())
 
         self.add_argument("--approach", type=str, choices=approaches, required=True)
 
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
 
-        result.update({
-            "APPROACH": self.args.approach,
-            self.args.approach: 1,
-        })
+        result["APPROACH"] = self.args.approach
+        result[self.args.approach] = 1
 
         return result
