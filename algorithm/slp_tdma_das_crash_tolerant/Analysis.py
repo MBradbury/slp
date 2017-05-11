b6c7b4b2565300a0d775a0abab0d3d8f4a3680b1
Jack Kirton <3943894+jack-kirton@users.noreply.github.com>
2017-05-11 16:54:26 +0100

    Clone SLP TDMA DAS to SLP TDMA DAS crash tolerant

---
+++ algorithm/slp_tdma_das_crash_tolerant/Analysis.py
@@ -0,0 +1,37 @@
+
+from data.analysis import AnalyzerCommon
+
+algorithm_module = __import__(__package__, globals(), locals(), ['object'], -1)
+
+class Analyzer(AnalyzerCommon):
+    def __init__(self, results_directory):
+        super(Analyzer, self).__init__(results_directory, self.results_header(), self.normalised_parameters())
+
+    @staticmethod
+    def normalised_parameters():
+        return (
+            ('Sent', 'TimeTaken'),
+            (('Sent', 'TimeTaken'), 'num_nodes'),
+        )
+
+    @staticmethod
+    def results_header():
+        d = AnalyzerCommon.common_results_header(algorithm_module.local_parameter_names)
+
+        AnalyzerCommon.common_results(d)
+
+        d['normal']             = lambda x: AnalyzerCommon._format_results(x, 'NormalSent')
+        d['empty normal']       = lambda x: AnalyzerCommon._format_results(x, 'EmptyNormalSent')
+        d['dissem']             = lambda x: AnalyzerCommon._format_results(x, 'DissemSent')
+        d['search']             = lambda x: AnalyzerCommon._format_results(x, 'SearchSent')
+        d['change']             = lambda x: AnalyzerCommon._format_results(x, 'ChangeSent')
+
+        d['node was source']    = lambda x: AnalyzerCommon._format_results(x, 'NodeWasSource', allow_missing=True)
+
+        d['sent heatmap']       = lambda x: AnalyzerCommon._format_results(x, 'SentHeatMap')
+        d['received heatmap']   = lambda x: AnalyzerCommon._format_results(x, 'ReceivedHeatMap')
+
+        d['norm(sent,time taken)'] = lambda x: AnalyzerCommon._format_results(x, 'norm(Sent,TimeTaken)')
+        d['norm(norm(sent,time taken),network size)'] = lambda x: AnalyzerCommon._format_results(x, 'norm(norm(Sent,TimeTaken),num_nodes)')
+
+        return d
