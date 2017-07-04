d6d8a2fcc11d79ab4942171d90f4b31ba4542cb6
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-07-04 13:23:09 +0100

    PO Flooding: Add cone width argument

--- algorithm/po_flooding/Arguments.py
+++ algorithm/po_flooding/Arguments.py
@@ -14,10 +14,12 @@ class Arguments(ArgumentsCommon):
             default=simulator.MobilityModel.StationaryMobilityModel())
 
         self.add_argument("--protected-sink-hops", type=self.type_positive_int, required=True)
+        self.add_argument("--cone-width", type=self.type_positive_int, required=True)
 
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
 
         result["PROTECTED_SINK_HOPS"] = self.args.protected_sink_hops
+        result["CONE_WIDTH"] = self.args.cone_width
 
         return result
