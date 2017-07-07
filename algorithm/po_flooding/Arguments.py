6d80f3009958f3b212906bb02aeb0b5a62c0e256
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-07-07 14:32:28 +0100

    PO Flooding: Work on randomly activating paths through the dead zone

--- algorithm/po_flooding/Arguments.py
+++ algorithm/po_flooding/Arguments.py
@@ -14,12 +14,12 @@ class Arguments(ArgumentsCommon):
                           default=simulator.MobilityModel.StationaryMobilityModel())
 
         self.add_argument("--protected-sink-hops", type=self.type_positive_int, required=True)
-        self.add_argument("--cone-width", type=self.type_positive_int, required=True)
+        self.add_argument("--activate-period", type=self.type_positive_float, required=True)
 
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
 
         result["PROTECTED_SINK_HOPS"] = self.args.protected_sink_hops
-        result["CONE_WIDTH"] = self.args.cone_width
+        result["ACTIVATE_PERIOD_MS"] = int(self.args.activate_period * 1000)
 
         return result
--- algorithm/po_flooding/Constants.h
+++ algorithm/po_flooding/Constants.h
@@ -6,12 +6,26 @@ enum Channels
 	NORMAL_CHANNEL = 1,
 	AWAY_CHANNEL = 2,
 	DISABLE_CHANNEL = 3,
+	ACTIVATE_CHANNEL = 4,
+	BEACON_CHANNEL = 5,
+	POLL_CHANNEL = 6,
 };
 
-#define SLP_MAX_NUM_SOURCES 20
+#define SLP_MAX_NUM_SOURCES 1
+#define SLP_MAX_NUM_SINKS 1
+
+#define SLP_MAX_1_HOP_NEIGHBOURHOOD 10
 
 #define AWAY_DELAY_MS 200
 
 #define SLP_OBJECT_DETECTOR_START_DELAY_MS (5 * 1000)
+
+#define CONE_WIDTH 3
+
+typedef struct
+{
+	int16_t sink_distance;
+	int16_t source_distance;
+} ni_container_t;
 
 #endif // SLP_CONSTANTS_H
