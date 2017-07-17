dd056032bb0ebc520c34acc31cbb6ee8397ef891
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-07-17 11:14:29 +0100

    POS Flooding: Add simple version of power off flooding

---
+++ algorithm/pos_flooding/Constants.h
@@ -0,0 +1,17 @@
+#ifndef SLP_CONSTANTS_H
+#define SLP_CONSTANTS_H
+
+enum Channels
+{
+	NORMAL_CHANNEL = 1,
+	AWAY_CHANNEL = 2,
+};
+
+#define SLP_MAX_NUM_SOURCES 1
+#define SLP_MAX_NUM_SINKS 1
+
+#define AWAY_DELAY_MS 200
+
+#define SLP_OBJECT_DETECTOR_START_DELAY_MS (5 * 1000)
+
+#endif // SLP_CONSTANTS_H
