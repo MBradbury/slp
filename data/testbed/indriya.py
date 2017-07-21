20a8cccaf8ddf802e22bfc19bab96ba84dd521ca
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-07-21 12:11:50 +0100

    Increase Indriya delay before boot

--- data/testbed/indriya.py
+++ data/testbed/indriya.py
@@ -23,7 +23,7 @@ def build_arguments():
     return {
         # Wait for a short amount of time before running the boot event.
         # This is to help catch all the serial output
-        "DELAYED_BOOT_TIME_MINUTES": 3
+        "DELAYED_BOOT_TIME_MINUTES": 12
     }
 
 def fastserial_supported():
