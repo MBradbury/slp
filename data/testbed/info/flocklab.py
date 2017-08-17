8306ff60d3bddd6776546bba95641894bae09715
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-08-17 12:27:43 +0100

    FlockLab: Add nodes 15 and 201 to the list of bad nodes

--- data/testbed/info/flocklab.py
+++ data/testbed/info/flocklab.py
@@ -24,7 +24,7 @@ class FlockLab(Topology):
         self.nodes[11] = np.array((561, 120), dtype=np.float64)
         self.nodes[13] = np.array((613, 293), dtype=np.float64)
         self.nodes[14] = np.array((628, 173), dtype=np.float64)
-        self.nodes[15] = np.array((128, 121), dtype=np.float64)
+        #self.nodes[15] = np.array((128, 121), dtype=np.float64)
         self.nodes[16] = np.array((85, 404), dtype=np.float64)
         self.nodes[17] = np.array((575, 333), dtype=np.float64)
         self.nodes[18] = np.array((233, 399), dtype=np.float64)
