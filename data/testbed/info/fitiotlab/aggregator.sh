9a524aa8ccee357a0955600e1b05b60bea718a2a
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-08-17 12:25:53 +0100

    When submitting jobs to fitiotlab, reset the nodes before starting the serial aggregator. This should ensure they boot at the same time.

--- data/testbed/info/fitiotlab/aggregator.sh
+++ data/testbed/info/fitiotlab/aggregator.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+trap 'catch_signal; exit' SIGTERM
+
+catch_signal()
+{
+	echo "Catch SIGTERM signal" >&2
+	#if [ "$#" -eq 1 ]
+	#then
+	#	echo "Source script config path: $1" >&2
+	#	source $1
+	#fi
+	gzip -9 ${OUTFILE}
+	gzip -9 ${ERRFILE}
+
+	mv ${OUTFILE}.gz ${OUTFILE}_$(hostname)_${EXP_ID}.gz
+	mv ${ERRFILE}.gz ${ERRFILE}_$(hostname)_${EXP_ID}.gz
+}
+
 #if [ "$#" -eq 1 ]
 #	then
 #	echo "Source script config path: $1" >&2
@@ -12,6 +29,9 @@ readonly OUTFILE="${HOME}/.iot-lab/${EXP_ID}/aggregator_log.stdout"
 readonly ERRFILE="${HOME}/.iot-lab/${EXP_ID}/aggregator_log.stderr"
 
 experiment-cli wait -i ${EXP_ID}
+
+# Reset all nodes, so they boot at the same time
+node-cli -i ${EXP_ID} --reset
 
 echo "Launch serial_aggregator with exp_id: ${EXP_ID}" >&2
 serial_aggregator -i ${EXP_ID} 2> ${ERRFILE} 1> ${OUTFILE}
