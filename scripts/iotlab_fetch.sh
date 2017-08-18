d87b0f1ff9c6303e09057c2e9739eaee5965c1d4
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-08-18 11:49:39 +0100

    fitiotlab: Add a script to help fetch the results

---
+++ scripts/iotlab_fetch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USER=$1
+SITE=$2
+TESTID=$3
+
+mkdir -p testbed_results/iotlab
+
+#experiment-cli get -i $TESTID -a
+
+#mv $TESTID.tar.gz testbed_results/iotlab/results.$TESTID.tar.gz
+
+#if [ ! -f "./testbed_results/iotlab/results.$TESTID.tar.gz" ]
+#then
+#	echo "Failed to fetch file"
+#	exit 1
+#fi
+
+#tar xzf testbed_results/iotlab/results.$TESTID.tar.gz --directory testbed_results/iotlab
+
+#rm testbed_results/iotlab/results.$TESTID.tar.gz
+
+# Remove submitted binaries
+#rm testbed_results/iotlab/$TESTID/*.ihex
+
+# Now to get some of the other files back from the testbed
+rsync -avz $USER@$SITE:~/.iot-lab/$TESTID/ testbed_results/iotlab/$TESTID/
+
+echo "Saved IoT Lab results to testbed_results/iotlab/$TESTID"
