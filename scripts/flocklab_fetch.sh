87e54b0a4ca70ccbb6c0fd703a682f2b1f2dd9eb
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-09-20 18:28:18 +0100

    Better status messages in flocklab_fetch.sh

--- scripts/flocklab_fetch.sh
+++ scripts/flocklab_fetch.sh
@@ -16,6 +16,8 @@ fi
 
 mkdir -p testbed_results/flocklab
 
+echo "Downloading results..."
+
 curl -s --user $USER:$PASSWORD $SERVER_URL/webdav/$TESTID/results.tar.gz --output testbed_results/flocklab/results.$TESTID.tar.gz
 
 if [ ! -f "./testbed_results/flocklab/results.$TESTID.tar.gz" ]
@@ -24,14 +26,20 @@ then
 	exit 1
 fi
 
+echo "Extracting results..."
+
 tar xzf testbed_results/flocklab/results.$TESTID.tar.gz --directory testbed_results/flocklab
+
+echo "Zipping power profiling..."
 
 # Compress the power profile, it tends to be very large!
 cd testbed_results/flocklab/$TESTID
 gzip --best powerprofiling.csv
-cd  -
+cd  - > /dev/null
 
 rm testbed_results/flocklab/results.$TESTID.tar.gz
+
+echo "Downloading configuration..."
 
 curl -s --user $USER:$PASSWORD $SERVER_URL/webdav/$TESTID/testconfiguration.xml --output testbed_results/flocklab/$TESTID/testconfiguration.xml
 
