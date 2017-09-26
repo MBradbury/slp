dd612d26f0a8baddd9896b4426eb1976e9748faa
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-09-26 14:01:30 +0100

    Allow the offline simulator to process multiple results files sequentially

--- simulator/sim/offline.py
+++ simulator/sim/offline.py
@@ -29,46 +29,47 @@ def run_simulation(module, a, count=1, print_warnings=False):
     else:
         raise RuntimeError("Unknown mode {}".format(a.args.mode))
 
-    with OfflineLogConverter.create_specific(a.args.log_converter, a.args.log_file) as converted_event_log:
-        with OfflineSimulation(module, configuration, a.args, event_log=converted_event_log) as sim:
+    for log_file in a.args.log_file:
+        with OfflineLogConverter.create_specific(a.args.log_converter, log_file) as converted_event_log:
+            with OfflineSimulation(module, configuration, a.args, event_log=converted_event_log) as sim:
 
-            # Create a copy of the provided attacker model
-            attacker = copy.deepcopy(a.args.attacker_model)
+                # Create a copy of the provided attacker model
+                attacker = copy.deepcopy(a.args.attacker_model)
 
-            # Setup each attacker model
-            attacker.setup(sim, configuration.sink_id, ident=0)
+                # Setup each attacker model
+                attacker.setup(sim, configuration.sink_id, ident=0)
 
-            sim.add_attacker(attacker)
+                sim.add_attacker(attacker)
 
-            try:
-                sim.run()
-            except Exception as ex:
-                import traceback
+                try:
+                    sim.run()
+                except Exception as ex:
+                    import traceback
 
-                all_args = "\n".join("{}={}".format(k, v) for (k, v) in vars(a.args).items() if k not in a.arguments_to_hide)
+                    all_args = "\n".join("{}={}".format(k, v) for (k, v) in vars(a.args).items() if k not in a.arguments_to_hide)
 
-                print("Killing run due to {}".format(ex), file=sys.stderr)
-                print(traceback.format_exc(), file=sys.stderr)
-                print("For parameters:", file=sys.stderr)
-                print(all_args, file=sys.stderr)
+                    print("Killing run due to {}".format(ex), file=sys.stderr)
+                    print(traceback.format_exc(), file=sys.stderr)
+                    print("For parameters:", file=sys.stderr)
+                    print(all_args, file=sys.stderr)
 
-                return 51
+                    return 51
 
-            try:
-                sim.metrics.print_results()
-            except Exception as ex:
-                import traceback
+                try:
+                    sim.metrics.print_results()
+                except Exception as ex:
+                    import traceback
 
-                all_args = "\n".join("{}={}".format(k, v) for (k, v) in vars(a.args).items() if k not in a.arguments_to_hide)
+                    all_args = "\n".join("{}={}".format(k, v) for (k, v) in vars(a.args).items() if k not in a.arguments_to_hide)
 
-                print("Failed to print metrics due to: {}".format(ex), file=sys.stderr)
-                print(traceback.format_exc(), file=sys.stderr)
-                print("For parameters:", file=sys.stderr)
-                print(all_args, file=sys.stderr)
-                
-                return 52
+                    print("Failed to print metrics due to: {}".format(ex), file=sys.stderr)
+                    print(traceback.format_exc(), file=sys.stderr)
+                    print("For parameters:", file=sys.stderr)
+                    print(all_args, file=sys.stderr)
+                    
+                    return 52
 
-            if print_warnings:
-                sim.metrics.print_warnings()
+                if print_warnings:
+                    sim.metrics.print_warnings()
 
     return 0
