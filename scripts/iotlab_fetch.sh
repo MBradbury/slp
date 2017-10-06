3420ca1adfaf03d55f8a67b8a19b037422f4d60a
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-10-06 15:40:01 +0100

    When fetching results from the testbeds make sure the directory they are going to be copied into exists

--- scripts/iotlab_fetch.sh
+++ scripts/iotlab_fetch.sh
@@ -9,26 +9,29 @@ mkdir -p testbed_results/iotlab
 # Now to get some of the other files back from the testbed
 rsync -avz --prune-empty-dirs $USER@$SITE:~/.iot-lab/$TESTID/ testbed_results/iotlab/$TESTID/
 
-sync
-
-if [ -d "testbed_results/iotlab/$TESTID" ]
+if [ ! -d "testbed_results/iotlab/$TESTID" ]
 then
-	cd "testbed_results/iotlab/$TESTID"
-
-	gunzip aggregator_log.stderr.gz
-	gunzip aggregator_log.stdout.gz
-
-	cd - > /dev/null
-
-	experiment-cli get -i $TESTID -p > testbed_results/iotlab/$TESTID/experiment.json
-
-    sync
-
-    name=$(jq -r '.name' testbed_results/iotlab/$TESTID/experiment.json)
-
-    new_name=$(echo "${name}_$TESTID" | sed '0,/-/s//\//')
-
-    mv "testbed_results/iotlab/$TESTID" "testbed_results/iotlab/${new_name}"
-
-	echo "Saved IoT Lab results to testbed_results/iotlab/${new_name}"
+    echo "The directory testbed_results/iotlab/$TESTID does not exist"
+    exit 1
 fi
+
+cd "testbed_results/iotlab/$TESTID"
+
+gunzip aggregator_log.stderr.gz
+gunzip aggregator_log.stdout.gz
+
+cd - > /dev/null
+
+experiment-cli get -i $TESTID -p > testbed_results/iotlab/$TESTID/experiment.json
+
+name=$(jq -r '.name' testbed_results/iotlab/$TESTID/experiment.json)
+
+new_name=$(echo "${name}_$TESTID" | sed '0,/-/s//\//')
+
+DIRNAME=$(dirname $new_name)
+
+mkdir -p "testbed_results/iotlab/$DIRNAME"
+
+mv "testbed_results/iotlab/$TESTID" "testbed_results/iotlab/${new_name}"
+
+echo "Saved IoT Lab results to testbed_results/iotlab/${new_name}"
