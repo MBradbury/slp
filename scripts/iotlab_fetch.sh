e0d8b27cc40f5e9efdc7fbb494c6bf8334ae1b48
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-10-09 13:36:17 +0100

    FIT IoT Lab: The testbed results directory should be fitiotlab instead of iotlab

--- scripts/iotlab_fetch.sh
+++ scripts/iotlab_fetch.sh
@@ -4,14 +4,16 @@ USER=$1
 SITE=$2
 TESTID=$3
 
-mkdir -p testbed_results/iotlab
+BASE_DIR="testbed_results/fitiotlab"
+
+mkdir -p "$BASE_DIR"
 
 # Now to get some of the other files back from the testbed
-rsync -avz --prune-empty-dirs $USER@$SITE:~/.iot-lab/$TESTID/ testbed_results/iotlab/$TESTID/
+rsync -avz --prune-empty-dirs $USER@$SITE:~/.iot-lab/$TESTID/ $BASE_DIR/$TESTID/
 
-if [ ! -d "testbed_results/iotlab/$TESTID" ]
+if [ ! -d "$BASE_DIR/$TESTID" ]
 then
-    echo "The directory testbed_results/iotlab/$TESTID does not exist"
+    echo "The directory $BASE_DIR/$TESTID does not exist"
     exit 1
 fi
 
@@ -22,16 +24,16 @@ gunzip aggregator_log.stdout.gz
 
 cd - > /dev/null
 
-experiment-cli get -i $TESTID -p > testbed_results/iotlab/$TESTID/experiment.json
+experiment-cli get -i $TESTID -p > $BASE_DIR/$TESTID/experiment.json
 
-name=$(jq -r '.name' testbed_results/iotlab/$TESTID/experiment.json)
+name=$(jq -r '.name' $BASE_DIR/$TESTID/experiment.json)
 
 new_name=$(echo "${name}_$TESTID" | sed '0,/-/s//\//')
 
 DIRNAME=$(dirname $new_name)
 
-mkdir -p "testbed_results/iotlab/$DIRNAME"
+mkdir -p "$BASE_DIR/$DIRNAME"
 
-mv "testbed_results/iotlab/$TESTID" "testbed_results/iotlab/${new_name}"
+mv "$BASE_DIR/$TESTID" "$BASE_DIR/${new_name}"
 
-echo "Saved IoT Lab results to testbed_results/iotlab/${new_name}"
+echo "Saved IoT Lab results to $BASE_DIR/${new_name}"
