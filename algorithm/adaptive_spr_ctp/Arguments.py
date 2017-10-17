26940eb47b4e3ad1ffc51ba6471dfdc232d6d545
Matthew Bradbury <matt-bradbury@live.co.uk>
2017-10-17 10:28:21 +0100

    AdaptiveSPR CTP: Update sample parameters and Arguments.py to match AdaptiveSPR Notify

--- algorithm/adaptive_spr_ctp/Arguments.py
+++ algorithm/adaptive_spr_ctp/Arguments.py
@@ -8,19 +8,17 @@ class Arguments(ArgumentsCommon):
     def __init__(self):
         super(Arguments, self).__init__("SLP Adaptive SPR CTP", has_safety_period=True)
 
-        self.add_argument("--source-period", type=float, required=True)
+        self.add_argument("--source-period", type=self.type_positive_float, required=True)
         self.add_argument("--source-mobility",
-                            type=simulator.MobilityModel.eval_input,
-                            default=simulator.MobilityModel.StationaryMobilityModel())
+                          type=simulator.MobilityModel.eval_input,
+                          default=simulator.MobilityModel.StationaryMobilityModel())
 
         self.add_argument("--approach", type=str, choices=approaches, required=True)
 
     def build_arguments(self):
         result = super(Arguments, self).build_arguments()
 
-        result.update({
-            "APPROACH": self.args.approach,
-            self.args.approach: 1,
-        })
+        result["APPROACH"] = self.args.approach
+        result[self.args.approach] = 1
 
         return result
