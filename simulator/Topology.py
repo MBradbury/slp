9bc924fb6980ccbcfc50c5cf63d6a00be838fb07
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-10-25 13:28:21 +0100

    Update Attacker to use OrderedId and add a __ne__ operator to the NodeId class

--- simulator/Topology.py
+++ simulator/Topology.py
@@ -28,6 +28,9 @@ class NodeId(object):
     def __eq__(self, other):
         return isinstance(other, type(self)) and self.nid == other.nid
 
+    def __ne__(self, other):
+        return not self == other
+
     def __repr__(self):
         return repr(self.nid)
 
