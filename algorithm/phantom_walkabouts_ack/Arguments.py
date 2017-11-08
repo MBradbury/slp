834bd743e665764deec8287cfd46ce65bb3455b6
Matthew Bradbury <MBradbury@users.noreply.github.com>
2017-11-08 12:00:28 +0000

    Phantom Walkabouts Ack: Reenable error logging and remove source period model

--- algorithm/phantom_walkabouts_ack/Arguments.py
+++ algorithm/phantom_walkabouts_ack/Arguments.py
@@ -17,7 +17,7 @@ class Arguments(ArgumentsCommon):
             has_safety_period=True, has_safety_factor=True)
 
         self.add_argument("--source-period",
-                          type=simulator.SourcePeriodModel.eval_input, required=True)
+                          type=self.type_positive_float, required=True)
         self.add_argument("--source-mobility",
                           type=simulator.MobilityModel.eval_input,
                           default=simulator.MobilityModel.StationaryMobilityModel())
