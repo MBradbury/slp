fbb787f8b3e1bcbebc4d67d2bce5cd6ab5158ff8
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2017-11-10 17:33:51 +0000

    Code tidyup

--- algorithm/_read_rssi/Arguments.py
+++ algorithm/_read_rssi/Arguments.py
@@ -5,4 +5,4 @@ class Arguments(ArgumentsCommon):
     def __init__(self):
         super(Arguments, self).__init__("RSSI", has_safety_period=False)
 
-        self.add_argument("--source-period", type=float, required=True)
+        self.add_argument("--source-period", type=self.type_positive_float, required=True)
