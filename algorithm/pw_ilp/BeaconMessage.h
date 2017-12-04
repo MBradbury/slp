d46c737d5a0727ec7070e53879c0df7e900483a0
Chen Gu <c.gu.warwick@gmail.com>
2017-12-04 11:17:56 +0000

    initial work on pw_ilp

---
+++ algorithm/pw_ilp/BeaconMessage.h
@@ -0,0 +1,17 @@
+#ifndef SLP_MESSAGES_BEACONMESSAGE_H
+#define SLP_MESSAGES_BEACONMESSAGE_H
+
+#include "HopDistance.h"
+
+typedef nx_struct
+{
+  nx_hop_distance_t sink_distance_of_sender;
+  nx_hop_distance_t source_distance_of_sender;
+  nx_hop_distance_t sink_source_distance;
+
+} BeaconMessage;
+
+inline SequenceNumberWithBottom Beacon_get_sequence_number(const BeaconMessage* msg) { return BOTTOM; }
+inline am_addr_t Beacon_get_source_id(const BeaconMessage* msg) { return AM_BROADCAST_ADDR; }
+
+#endif // SLP_MESSAGES_BEACONMESSAGE_H
