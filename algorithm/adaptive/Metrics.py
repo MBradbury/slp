b542d143cea4b5ea2eb7e613982d0d2a59e19b38
Matthew Bradbury <matt-bradbury@live.co.uk>
2018-01-17 15:31:07 +0000

    Adaptive: Record duty cycle metrics

--- algorithm/adaptive/Metrics.py
+++ algorithm/adaptive/Metrics.py
@@ -1,11 +1,10 @@
-from __future__ import print_function
 
-from simulator.MetricsCommon import MetricsCommon, FakeMetricsCommon
+from simulator.MetricsCommon import MetricsCommon, FakeMetricsCommon, DutyCycleMetricsCommon
 
-class Metrics(FakeMetricsCommon):
+class Metrics(FakeMetricsCommon, DutyCycleMetricsCommon):
 
     def __init__(self, *args, **kwargs):
-        super(Metrics, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     @staticmethod
     def items():
@@ -14,5 +13,6 @@ class Metrics(FakeMetricsCommon):
         d["AwaySent"]               = lambda x: x.number_sent("Away")
 
         d.update(FakeMetricsCommon.items({"TFS": "TempFakeNode", "PFS": "PermFakeNode"}))
+        d.update(DutyCycleMetricsCommon.items())
 
         return d
