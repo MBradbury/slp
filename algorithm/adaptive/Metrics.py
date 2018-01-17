4dc461851cbfcdccede14117d167d063ef58673f
Matthew Bradbury <MBradbury@users.noreply.github.com>
2018-01-17 19:56:59 +0000

    Adaptive: Use a Notify message to start Fake allocation and port other improvements from AdaptiveSPR

--- algorithm/adaptive/Metrics.py
+++ algorithm/adaptive/Metrics.py
@@ -11,6 +11,7 @@ class Metrics(FakeMetricsCommon, DutyCycleMetricsCommon):
         d = MetricsCommon.items()
         d["ChooseSent"]             = lambda x: x.number_sent("Choose")
         d["AwaySent"]               = lambda x: x.number_sent("Away")
+        d["NotifySent"]             = lambda x: x.number_sent("Notify")
 
         d.update(FakeMetricsCommon.items({"TFS": "TempFakeNode", "PFS": "PermFakeNode"}))
         d.update(DutyCycleMetricsCommon.items())
