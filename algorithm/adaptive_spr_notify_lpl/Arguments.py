2395c5e9d66f1f5d156c644b9f3a8b349891c156
Matthew Bradbury <matt-bradbury@live.co.uk>
2018-02-26 13:31:56 +0000

    AdaptiveSPR Notify LPL: Fix arguments type

--- algorithm/adaptive_spr_notify_lpl/Arguments.py
+++ algorithm/adaptive_spr_notify_lpl/Arguments.py
@@ -5,12 +5,12 @@ class Arguments(ArgumentsCommon):
     def __init__(self):
         super().__init__()
 
-        self.add_argument("--lpl-choose-early", type=self.type_positive_float, required=True) #   5
-        self.add_argument("--lpl-choose-late", type=self.type_positive_float, required=True)  #  40
-        self.add_argument("--lpl-normal-early", type=self.type_positive_float, required=True) #  40
-        self.add_argument("--lpl-normal-late", type=self.type_positive_float, required=True)  #  40
-        self.add_argument("--lpl-fake-early", type=self.type_positive_float, required=True)   # 100
-        self.add_argument("--lpl-fake-late", type=self.type_positive_float, required=True)    # 150
+        self.add_argument("--lpl-choose-early", type=self.type_positive_int, required=True) #   5
+        self.add_argument("--lpl-choose-late", type=self.type_positive_int, required=True)  #  40
+        self.add_argument("--lpl-normal-early", type=self.type_positive_int, required=True) #  40
+        self.add_argument("--lpl-normal-late", type=self.type_positive_int, required=True)  #  40
+        self.add_argument("--lpl-fake-early", type=self.type_positive_int, required=True)   # 100
+        self.add_argument("--lpl-fake-late", type=self.type_positive_int, required=True)    # 150
 
     def parse(self, argv):
         super().parse(argv)
@@ -22,11 +22,11 @@ class Arguments(ArgumentsCommon):
     def build_arguments(self):
         result = super().build_arguments()
 
-        result["SLP_LPL_CHOOSE_EARLY_MS"] = int(self.args.lpl_choose_early)
-        result["SLP_LPL_CHOOSE_LATE_MS"] = int(self.args.lpl_choose_late)
-        result["SLP_LPL_NORMAL_EARLY_MS"] = int(self.args.lpl_normal_early)
-        result["SLP_LPL_NORMAL_LATE_MS"] = int(self.args.lpl_normal_late)
-        result["SLP_LPL_FAKE_EARLY_MS"] = int(self.args.lpl_fake_early)
-        result["SLP_LPL_FAKE_LATE_MS"] = int(self.args.lpl_fake_late)
+        result["SLP_LPL_CHOOSE_EARLY_MS"] = self.args.lpl_choose_early
+        result["SLP_LPL_CHOOSE_LATE_MS"] = self.args.lpl_choose_late
+        result["SLP_LPL_NORMAL_EARLY_MS"] = self.args.lpl_normal_early
+        result["SLP_LPL_NORMAL_LATE_MS"] = self.args.lpl_normal_late
+        result["SLP_LPL_FAKE_EARLY_MS"] = self.args.lpl_fake_early
+        result["SLP_LPL_FAKE_LATE_MS"] = self.args.lpl_fake_late
 
         return result
