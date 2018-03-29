6954ba00e2c71ccbcda7437f064788ce306e58bf
Jack Kirton <jack.d.kirton@gmail.com>
2018-03-29 15:46:44 +0100

    Protectionless TDMA DAS: Update to working order

--- algorithm/protectionless_tdma_das/__init__.py
+++ algorithm/protectionless_tdma_das/__init__.py
@@ -7,4 +7,5 @@ def _setup():
 (name, results_path, result_file, result_file_path, graphs_path) = _setup()
 
 
-local_parameter_names = ('slot period', 'dissem period', 'tdma num slots', 'slot assignment interval', 'minimum setup periods', 'pre beacon periods', "dissem timeout")
+# local_parameter_names = ('slot period', 'dissem period', 'tdma num slots', 'slot assignment interval', 'minimum setup periods', 'pre beacon periods', "dissem timeout", 'timesync', 'timesync period')
+local_parameter_names = ('slot period', 'dissem period', 'tdma num slots', 'slot assignment interval', 'minimum setup periods', 'pre beacon periods', 'timesync', 'timesync period')
