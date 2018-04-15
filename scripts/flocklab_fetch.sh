7d809dd7da9cee3b9854e737355e00112a15eb06
Matthew Bradbury <matt-bradbury@live.co.uk>
2018-04-15 09:10:54 +0100

    Improve flocklab_fetch to skip existing experiments

--- scripts/flocklab_fetch.sh
+++ scripts/flocklab_fetch.sh
@@ -14,7 +14,31 @@ else
   fi
 fi
 
-mkdir -p testbed_results/flocklab
+mkdir -p "testbed_results/flocklab"
+rm -f "testbed_results/flocklab/testconfiguration.xml"
+
+echo "Downloading ${TESTID} configuration..."
+
+curl -s -S -w "%{http_code}" --user $USER:$PASSWORD $SERVER_URL/webdav/$TESTID/testconfiguration.xml --output testbed_results/flocklab/testconfiguration.xml
+
+if [ ! -f "testbed_results/flocklab/testconfiguration.xml" ]
+then
+    echo ": Failed to fetch file for $TESTID/testconfiguration.xml"
+    exit 1
+fi
+
+echo ": Processing results..."
+
+NAME=$(cat testbed_results/flocklab/testconfiguration.xml | sed 's/xmlns=".*"//g' | xmllint --xpath '/testConf/generalConf/description/text()' - )
+DIRNAME=$(dirname $NAME)
+SAVEPATH="testbed_results/flocklab/${NAME}_$TESTID"
+
+if [ -d "$SAVEPATH" ]
+then
+    rm -f "testbed_results/flocklab/testconfiguration.xml"
+    echo "Already have results for $TESTID, so skipping it."
+    exit 2
+fi
 
 echo "Downloading results for ${TESTID}..."
 
@@ -22,6 +46,7 @@ curl -s -S -w "%{http_code}" --user $USER:$PASSWORD $SERVER_URL/webdav/$TESTID/r
 
 if [ ! -f "./testbed_results/flocklab/results.$TESTID.tar.gz" ]
 then
+    rm -f "testbed_results/flocklab/testconfiguration.xml"
 	echo ": Failed to fetch file for $TESTID"
 	exit 1
 fi
@@ -39,16 +64,10 @@ cd  - > /dev/null
 
 rm testbed_results/flocklab/results.$TESTID.tar.gz
 
-echo "Downloading configuration..."
-
-curl -s --user $USER:$PASSWORD $SERVER_URL/webdav/$TESTID/testconfiguration.xml --output testbed_results/flocklab/$TESTID/testconfiguration.xml
-
-NAME=$(cat testbed_results/flocklab/$TESTID/testconfiguration.xml | sed 's/xmlns=".*"//g' | xmllint --xpath '/testConf/generalConf/description/text()' - )
-
-DIRNAME=$(dirname $NAME)
+mv testbed_results/flocklab/testconfiguration.xml testbed_results/flocklab/$TESTID/testconfiguration.xml
 
 mkdir -p "testbed_results/flocklab/$DIRNAME"
 
-mv "testbed_results/flocklab/$TESTID" "testbed_results/flocklab/${NAME}_$TESTID"
+mv "testbed_results/flocklab/$TESTID" "$SAVEPATH"
 
-echo "Saved FlockLab results $TESTID to testbed_results/flocklab/${NAME}_$TESTID"
+echo "Saved FlockLab results $TESTID to $SAVEPATH"
