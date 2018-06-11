4b8ed4b95b5e6aaf184a9d8616c76cc08ccd0cd7
Jack Kirton <3943894+jack-kirton@users.noreply.github.com>
2018-06-11 18:00:41 +0100

    Protectionless TDMA DAS: Add sequence numbers to EmptyNormal messages

--- algorithm/protectionless_tdma_das/EmptyNormalMessage.h
+++ algorithm/protectionless_tdma_das/EmptyNormalMessage.h
@@ -1,14 +1,15 @@
 #ifndef SLP_MESSAGES_EMPTYNORMALMESSAGE_H
 #define SLP_MESSAGES_EMPTYNORMALMESSAGE_H
 
-//#include "SequenceNumber.h"
+#include "SequenceNumber.h"
 #include "NormalMessage.h"
 
 typedef nx_struct EmptyNormalMessage {
-    nx_uint8_t state[sizeof(NormalMessage)];
+    NXSequenceNumber sequence_number;
+    nx_uint8_t state[sizeof(NormalMessage) - sizeof(NXSequenceNumber)];
 } EmptyNormalMessage;
 
-inline SequenceNumberWithBottom EmptyNormal_get_sequence_number(const EmptyNormalMessage* msg) { return BOTTOM; }
+inline SequenceNumberWithBottom EmptyNormal_get_sequence_number(const EmptyNormalMessage* msg) { return msg->sequence_number; }
 inline am_addr_t EmptyNormal_get_source_id(const EmptyNormalMessage* msg) { return AM_BROADCAST_ADDR; }
 
 #endif // SLP_MESSAGES_EMPTYNORMALMESSAGE_H
