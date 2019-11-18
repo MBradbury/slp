1849e4d899aabb1f6bd49e449358e0de24bcfe27
Matthew Bradbury <matt-bradbury@live.co.uk>
2019-11-18 14:56:53 +0000

    Phantom: Correct argument product

--- algorithm/phantom/CommandLine.py
+++ algorithm/phantom/CommandLine.py
@@ -1,43 +1,49 @@
-from __future__ import print_function
 
 import itertools
 import os
 
+import simulator.sim
 from simulator import CommandLineCommon
 
 import algorithm
-
 protectionless = algorithm.import_algorithm("protectionless")
-adaptive = algorithm.import_algorithm("adaptive")
 
-from data import results
-
-from data.table import safety_period, fake_result
+from data import submodule_loader, results
+from data.table import fake_result
 from data.graph import summary, versus, min_max_versus, dual_min_max_versus
 from data.util import scalar_extractor
 
 class CLI(CommandLineCommon.CLI):
     def __init__(self):
-        super(CLI, self).__init__(protectionless.name)
+        super().__init__(protectionless.name)
 
         subparser = self._add_argument("table", self._run_table)
+        subparser.add_argument("sim", choices=submodule_loader.list_available(simulator.sim), help="The simulator you wish to run with.")
+        subparser.add_argument("--show", action="store_true", default=False)
+
         subparser = self._add_argument("graph", self._run_graph)
+        subparser.add_argument("sim", choices=submodule_loader.list_available(simulator.sim), help="The simulator you wish to run with.")
+
         subparser = self._add_argument("min-max-versus", self._run_min_max_versus)
-        subparser = self._add_argument("dual-min-max-versus", self._run_dual_min_max_versus)
 
     def _argument_product(self, sim, extras=None):
         parameters = self.algorithm_module.Parameters
 
-        argument_product = list(filter(
-            lambda x: x[10] in parameters.walk_hop_lengths[x[0]],
-            itertools.product(
-                parameters.sizes, parameters.configurations,
-                parameters.attacker_models, parameters.noise_models, parameters.communication_models, parameters.fault_models,
-                [parameters.distance], parameters.node_id_orders, [parameters.latest_node_start_time],
-                parameters.source_periods,
-                set(itertools.chain(*parameters.walk_hop_lengths.values())), parameters.landmark_nodes
-            )
-        ))
+        # Some complexity as the walk hop length is determined by the network size
+        network_size_index = sim.global_parameter_names.index('network size')
+        walk_hop_length_index = len(sim.global_parameter_names)
+
+        parameter_values = self._get_global_parameter_values(sim, parameters)
+        parameter_values.append(set(itertools.chain(*parameters.walk_hop_lengths.values())))
+        parameter_values.append(parameters.landmark_nodes)
+
+        argument_product = list(itertools.product(*parameter_values))
+
+        # Remove incorrect combinations of walk hop lengths
+        argument_product = [
+            x for x in argument_product
+            if x[walk_hop_length_index] in parameters.walk_hop_lengths[x[network_size_index]]
+        ]
 
         argument_product = self.add_extra_arguments(argument_product, extras)
 
@@ -211,93 +217,6 @@ class CLI(CommandLineCommon.CLI):
         for result_name in graph_parameters.keys():
             graph_min_max_versus(result_name)
 
-    def _run_dual_min_max_versus(self, args):
-        graph_parameters = {
-            ('norm(norm(sent,time taken),num_nodes)', 'energy allowance used'): ('Total Messages Sent per node per second', 'Energy Allowance Used (\%)', 'right top'),
-        }
-
-        sample_energy_allowance_used = 23.2076127193
-        sample_sent_per_node_per_sec = 4.28833268899
-
-        custom_yaxis_range_max = {
-            # Calculated so that the scale matches the energy allowance used scale exactly using two reference values
-            'norm(norm(sent,time taken),num_nodes)': sample_sent_per_node_per_sec / (sample_energy_allowance_used / 100),
-
-            'energy allowance used': 100,
-        }
-
-        results_to_load = [param for sublist in graph_parameters.keys() for param in sublist]
-
-        protectionless_results = results.Results(
-            protectionless.result_file_path,
-            parameters=tuple(),
-            results=results_to_load,
-            network_size_normalisation="UseNumNodes"
-        )
-
-        adaptive_results = results.Results(
-            adaptive.result_file_path,
-            parameters=('approach',),
-            results=results_to_load,
-            network_size_normalisation="UseNumNodes"
-        )
-
-        phantom_results = results.Results(
-            self.algorithm_module.result_file_path,
-            parameters=self.algorithm_module.local_parameter_names,
-            results=results_to_load,
-            network_size_normalisation="UseNumNodes"
-        )
-
-        def graph_dual_min_max_versus(result_name1, result_name2, xaxis):
-            name = 'dual-min-max-{}-versus-{}_{}-{}'.format(adaptive.name, result_name1, result_name2, xaxis)
-
-            g = dual_min_max_versus.Grapher(
-                self.algorithm_module.graphs_path, name,
-                xaxis=xaxis, yaxis1=result_name1, yaxis2=result_name2, vary='walk length', yextractor=scalar_extractor)
-
-            g.xaxis_label = xaxis.title()
-            g.yaxis1_label = graph_parameters[(result_name1, result_name2)][0]
-            g.yaxis2_label = graph_parameters[(result_name1, result_name2)][1]
-            g.key_position = graph_parameters[(result_name1, result_name2)][2]
-
-            g.yaxis_font = g.xaxis_font = "',15'"
-
-            g.nokey = True
-
-            g.generate_legend_graph = True
-
-            g.point_size = 1.3
-            g.line_width = 4
-            g.yaxis_font = "',14'"
-            g.xaxis_font = "',12'"
-
-            g.min_label = 'Dynamic - Lowest'
-            g.max_label = 'Dynamic - Highest'
-            g.comparison_label = 'Phantom'
-            g.baseline_label = 'Protectionless - Baseline'
-            g.vary_label = ''
-
-            g.only_show_yaxis1 = True
-
-            if result_name1 in custom_yaxis_range_max:
-                g.yaxis1_range_max = custom_yaxis_range_max[result_name1]
-
-            if result_name2 in custom_yaxis_range_max:
-                g.yaxis2_range_max = custom_yaxis_range_max[result_name2]
-
-            g.vvalue_label_converter = lambda value: "W_h = {}".format(value)
-
-            g.create(adaptive_results, phantom_results, baseline_results=protectionless_results)
-
-            summary.GraphSummary(
-                os.path.join(self.algorithm_module.graphs_path, name),
-                os.path.join(algorithm.results_directory_name, '{}-{}'.format(self.algorithm_module.name, name).replace(" ", "_"))
-            ).run()
-
-        for (result_name1, result_name2) in graph_parameters.keys():
-            graph_dual_min_max_versus(result_name1, result_name2, 'network size')
-
     def run(self, args):
         args = super(CLI, self).run(args)
 
@@ -309,6 +228,3 @@ class CLI(CommandLineCommon.CLI):
 
         if 'min-max-versus' == args.mode:
             self._run_min_max_versus(args)
-
-        if 'dual-min-max-versus' == args.mode:
-            self._run_dual_min_max_versus(args)
