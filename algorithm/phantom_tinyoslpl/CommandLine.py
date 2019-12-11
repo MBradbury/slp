4f6eb5571281e8c051b6bf6b2b3016678fe27cd3
Matthew Bradbury <MBradbury@users.noreply.github.com>
2019-12-11 16:49:54 +0000

    Phantom TinyOS LPL: Add grapher

--- algorithm/phantom_tinyoslpl/CommandLine.py
+++ algorithm/phantom_tinyoslpl/CommandLine.py
@@ -7,6 +7,7 @@ from simulator import CommandLineCommon
 
 import algorithm
 protectionless = algorithm.import_algorithm("protectionless")
+phantom = algorithm.import_algorithm("phantom", extras=["Analysis"])
 
 from data import submodule_loader, results
 from data.table import fake_result
@@ -27,8 +28,6 @@ class CLI(CommandLineCommon.CLI):
 
         subparser = self._add_argument("graph", self._run_graph)
         subparser.add_argument("sim", choices=submodule_loader.list_available(simulator.sim), help="The simulator you wish to run with.")
-
-        subparser = self._add_argument("min-max-versus", self._run_min_max_versus)
 
     def _argument_product(self, sim, extras=None):
         parameters = self.algorithm_module.Parameters
@@ -101,160 +100,76 @@ class CLI(CommandLineCommon.CLI):
     def _run_graph(self, args):
         graph_parameters = {
             'normal latency': ('Normal Message Latency (ms)', 'left top'),
-            'ssd': ('Sink-Source Distance (hops)', 'left top'),
-            'captured': ('Capture Ratio (%)', 'right top'),
-            'sent': ('Total Messages Sent', 'left top'),
+            #'ssd': ('Sink-Source Distance (hops)', 'left top'),
+            'captured': ('Capture Ratio (%)', 'left top'),
+            #'sent': ('Total Messages Sent', 'left top'),
             'received ratio': ('Receive Ratio (%)', 'left bottom'),
-            'paths reached end': ('Paths Reached End (%)', 'right top'),
-            'source dropped': ('Source Dropped Messages (%)', 'right top'),
+            #'tfs': ('Number of TFS Created', 'left top'),
+            #'pfs': ('Number of PFS Created', 'left top'),
+            #'tailfs': ('Number of TailFS Created', 'left top'),
+            'attacker distance': ('Attacker-Source Distance (Meters)', 'left top'),
+            #"attacker distance percentage": ('Normalised Attacker Distance (%)', 'left top'),
+            'average duty cycle': ('Average Duty Cycle (%)', 'right top'),
+            #'norm(norm(sent,time taken),network size)': ('Messages Sent per Sec per Node', 'left top'),
         }
 
-        custom_yaxis_range_max = {
-            'source dropped': 100,
-            'paths reached end': 100,
-        }
+        lpl_params = self.algorithm_module.extra_parameter_names
 
-        phantom_results = results.Results(
-            self.algorithm_module.result_file_path,
-            parameters=self.algorithm_module.local_parameter_names,
-            results=tuple(graph_parameters.keys()),
-            network_size_normalisation="UseNumNodes"
-        )
-
-        parameters = [
-            ('source period', ' seconds'),
-            ('walk length', ' hops')
+        varying = [
+            #(('network size', ''), ('source period', ' seconds')),
+            #(('network size', ''), (lpl_params, '~')),
+            (('source period', ''), (lpl_params, '~')),
         ]
 
-        for (parameter_name, parameter_unit) in parameters:
-            for (yaxis, (yaxis_label, key_position)) in graph_parameters.items():
-                name = '{}-v-{}'.format(yaxis.replace(" ", "_"), parameter_name.replace(" ", "-"))
-
-                g = versus.Grapher(
-                    self.algorithm_module.graphs_path, name,
-                    xaxis='network size', yaxis=yaxis, vary=parameter_name,
-                    yextractor=scalar_extractor
-                )
-
-                g.xaxis_label = 'Number of Nodes'
-                g.yaxis_label = yaxis_label
-                g.vary_label = parameter_name.title()
-                g.vary_prefix = parameter_unit
-                g.key_position = key_position
-
-                g.nokey = True
-
-                g.generate_legend_graph = True
-
-                g.point_size = 1.3
-                g.line_width = 4
-                g.yaxis_font = "',14'"
-                g.xaxis_font = "',12'"
-
-                if yaxis in custom_yaxis_range_max:
-                    g.yaxis_range_max = custom_yaxis_range_max[yaxis]
-
-                g.create(phantom_results)
-
-                summary.GraphSummary(
-                    os.path.join(self.algorithm_module.graphs_path, name),
-                    os.path.join(algorithm.results_directory_name, self.algorithm_module.name + '-' + name)
-                ).run()
-
-    def _run_min_max_versus(self, args):
-        graph_parameters = {
-            'normal latency': ('Normal Message Latency (ms)', 'at 17.5,290'),
-            'ssd': ('Sink-Source Distance (hops)', 'left top'),
-            'captured': ('Capture Ratio (%)', 'right top'),
-            'sent': ('Total Messages Sent', 'left top'),
-            'norm(norm(sent,time taken),num_nodes)': ('Total Messages Sent per node per second', 'left top'),
-            'received ratio': ('Receive Ratio (%)', 'right top'),
-            'energy impact per node per second': ('Energy Impact per Node per second (mAh s^{-1})', 'left top'),
-            'energy allowance used': ('Energy Allowance Used (\%)', 'left top'),
-        }
-
         custom_yaxis_range_max = {
-            'sent': 450000,
-            'captured': 40,
+            'captured': 35,
             'received ratio': 100,
-            'normal latency': 300,
-            'norm(norm(sent,time taken),num_nodes)': 30,
-            'energy allowance used': 100,
+            'average duty cycle': 100,
+            'normal latency': 3000,
+            'attacker distance': 70,
+            #'norm(norm(sent,time taken),network size)': 5,
         }
 
-        nokey = {'captured', 'sent', 'received ratio',
-                 'norm(norm(sent,time taken),num_nodes)', 'energy allowance used'}
+        #custom_yaxis_range_min = {
+        #    'received ratio': 70,
+        #}
 
-        protectionless_results = results.Results(
-            protectionless.result_file_path,
-            parameters=tuple(),
-            results=graph_parameters.keys(),
-            network_size_normalisation="UseNumNodes"
+        yextractors = {
+            # Just get the distance of attacker 0 from node 0 (the source in SourceCorner)
+            "attacker distance": lambda yvalue: scalar_extractor(yvalue, key=(0, 0))
+        }
+
+        def fetch_baseline_result(baseline_results, data_key, src_period, baseline_params):
+
+            if data_key[-1] != 'enabled':
+                raise RuntimeError(f"Expected 'enabled', got {data_key[-1]}")
+
+            # adaptive_spr_notify doesn't run with lpl enabled, but that is what we want to compare against
+            data_key = data_key[:-1] + ('disabled',)
+
+            return baseline_results.data[data_key][src_period][baseline_params]
+
+        def filter_params(all_params):
+            return all_params['source period'] == '0.25'
+
+        self._create_baseline_versus_graph(args.sim, phantom, graph_parameters, varying,
+            results_filter=filter_params,
+            custom_yaxis_range_max=custom_yaxis_range_max,
+            #custom_yaxis_range_min=custom_yaxis_range_min,
+            yextractor = yextractors,
+            xaxis_font = "',18'",
+            yaxis_font = "',18'",
+            xlabel_font = "',16'",
+            ylabel_font = "',15'",
+            line_width = 3,
+            point_size = 1,
+            nokey = False,#True,
+            generate_legend_graph = True,
+            legend_font_size = 16,
+            legend_divisor = 4,
+            legend_base_height = 0.3,
+            vary_label = "",
+            baseline_label="Phantom (no duty cycle)",
+
+            fetch_baseline_result=fetch_baseline_result,
         )
-
-        adaptive_results = results.Results(
-            adaptive.result_file_path,
-            parameters=('approach',),
-            results=graph_parameters.keys(),
-            network_size_normalisation="UseNumNodes"
-        )
-
-        phantom_results = results.Results(
-            self.algorithm_module.result_file_path,
-            parameters=self.algorithm_module.local_parameter_names,
-            results=graph_parameters.keys(),
-            network_size_normalisation="UseNumNodes"
-        )
-
-        def graph_min_max_versus(result_name):
-            name = 'min-max-{}-versus-{}'.format(result_name, adaptive.name)
-
-            g = min_max_versus.Grapher(
-                self.algorithm_module.graphs_path, name,
-                xaxis='network size', yaxis=result_name, vary='walk length', yextractor=scalar_extractor)
-
-            g.xaxis_label = 'Number of Nodes'
-            g.yaxis_label = graph_parameters[result_name][0]
-            g.key_position = graph_parameters[result_name][1]
-
-            g.nokey = result_name in nokey
-
-            g.min_label = 'Dynamic - Lowest'
-            g.max_label = 'Dynamic - Highest'
-            g.comparison_label = 'Phantom'
-            g.baseline_label = 'Protectionless - Baseline'
-            g.vary_label = ''
-
-            g.generate_legend_graph = True
-
-            g.point_size = 1.3
-            g.line_width = 4
-            g.yaxis_font = "',14'"
-            g.xaxis_font = "',12'"
-
-            if result_name in custom_yaxis_range_max:
-                g.yaxis_range_max = custom_yaxis_range_max[result_name]
-
-            g.vvalue_label_converter = lambda value: "W_h = {}".format(value)
-
-            g.create(adaptive_results, phantom_results, protectionless_results)
-
-            summary.GraphSummary(
-                os.path.join(self.algorithm_module.graphs_path, name),
-                os.path.join(algorithm.results_directory_name, '{}-{}'.format(self.algorithm_module.name, name).replace(" ", "_"))
-            ).run()
-
-        for result_name in graph_parameters.keys():
-            graph_min_max_versus(result_name)
-
-    def run(self, args):
-        args = super(CLI, self).run(args)
-
-        if 'table' == args.mode:
-            self._run_table(args)
-
-        if 'graph' == args.mode:
-            self._run_graph(args)
-
-        if 'min-max-versus' == args.mode:
-            self._run_min_max_versus(args)
