cda15422c063da0a4605d02060ce46781c1da049
Matthew Bradbury <matt-bradbury@live.co.uk>
2019-12-13 22:06:03 +0000

    Fix scripts/job-progress.sh to only check the progress of running jobs

--- scripts/job-progress.sh
+++ scripts/job-progress.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python scripts/${1}-qstat.py ${@:2} | grep R | column -t | tr -s ' ' | cut -d' ' -f 3 | tr . / | awk '{print $0 ".txt"}' | xargs wc -l
+python scripts/${1}-qstat.py ${@:2} | grep " R " | column -t | tr -s ' ' | cut -d' ' -f 3 | tr . / | awk '{print $0 ".txt"}' | xargs wc -l
