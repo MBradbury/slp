177943ee8b3a525d656369ba72d1f2119d7104a1
Matthew Bradbury <MBradbury@users.noreply.github.com>
2019-12-20 10:47:40 +0000

    When running locally, check if results would be overwritten and abort if they would.

--- data/cycle_accurate/cooja.py
+++ data/cycle_accurate/cooja.py
@@ -96,7 +96,7 @@ def create_csc(csc, target_directory, a):
         seconds_to_run = configuration.size() * 4.0 * slowest_source_period
 
     # Many algorithms have some sort of setup period, so it is important to allow cooja to consider some time for this
-    # Try getting this value from the algorithm itself, otherwise guess 10 seconds
+    # Try getting this value from the algorithm itself, otherwise guess 15 seconds
     seconds_to_run += getattr(a, "cycle_accurate_setup_period", 15.0)
 
     # See: https://github.com/contiki-os/contiki/wiki/Using-Cooja-Test-Scripts-to-Automate-Simulations
