d80c25164b0e0e99e229c37ed543bec5be3bee5b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-23 12:44:04 +0800

    Delete simulator directory

--- simulator/__init__.py
+++
@@ -1,9 +0,0 @@
-
-try:
-    import lazy_import
-    lazy_import.lazy_module("pandas")
-    lazy_import.lazy_module("base64")
-    lazy_import.lazy_module("re")
-    lazy_import.lazy_module("pickle")
-except ImportError:
-    pass
--- simulator/topovis/__init__.py
+++
@@ -1,5 +0,0 @@
-from simulator.topovis.TopoVis import *
-from simulator.topovis.common import Parameters
-
-__all__ = ['LineStyle', 'FillStyle', 'TextStyle', 'Node', 'Scene',
-           'GenericPlotter', 'Parameters']
